981e098d53aca4fe6708fde2619cf48b0f9a52dc
Gilles Caulier <caulier.gilles@gmail.com>
2004-06-24 09:23:04 +0000

    In according with Jesper : - Added new KIPI features AlbumsHaveCategory and AlbumsHaveCreationDate requested for ImagesGallery an CDArchiving plugins. - Added new virtuals methods in KIPI::ImageCollection and KIPI::ImageCollectionShared class in according with this new features. CCMAIL: kde-imaging@kde.org
    svn path=/trunk/kdeextragear-libs-1/kipi-plugins/; revision=323282

--- helloworld/plugin_helloworld.cpp
+++ helloworld/plugin_helloworld.cpp
@@ -56,6 +56,7 @@ Plugin_HelloWorld::Plugin_HelloWorld(QObject *parent,
 void Plugin_HelloWorld::setup( QWidget* widget )
 {
     KIPI::Plugin::setup( widget );
+    
     // this is our action shown in the menubar/toolbar of the mainwindow
     m_actionHelloWorld = new KAction (i18n("Hello World..."),
                                    "misc",
@@ -64,6 +65,7 @@ void Plugin_HelloWorld::setup( QWidget* widget )
                                    SLOT(slotActivate()),
                                    actionCollection(),
                                    "helloworld");
+    
     addAction( m_actionHelloWorld );
 
     m_interface = dynamic_cast< KIPI::Interface* >( parent() );
@@ -80,27 +82,61 @@ void Plugin_HelloWorld::slotActivate()
     kdDebug( 51000 ) << "Plugin_HelloWorld slot activated" << endl;
 
     // Print some information about the capabilities of the host application.
-    kdDebug( 51000 ) << "Features supported by the host application:" << endl;
-    kdDebug( 51000 ) << "  AlbumsHaveComments:  " << (m_interface->hasFeature( KIPI::AlbumsHaveComments ) ? "Yes" : "No") << endl;
-    kdDebug( 51000 ) << "  AlbumEQDir:          " << (m_interface->hasFeature( KIPI::AlbumEQDir ) ? "Yes" : "No") << endl;
-    kdDebug( 51000 ) << "  ImagesHasComments:   " << (m_interface->hasFeature( KIPI::ImagesHasComments ) ? "Yes" : "No") << endl;
-    kdDebug( 51000 ) << "  ImagesHasTime:       " << (m_interface->hasFeature( KIPI::ImagesHasTime ) ? "Yes" : "No") << endl;
-    kdDebug( 51000 ) << "  SupportsDateRanges:  " << (m_interface->hasFeature( KIPI::SupportsDateRanges ) ? "Yes" : "No") << endl;
-    kdDebug( 51000 ) << "  AcceptNewImages:     " << (m_interface->hasFeature( KIPI::AcceptNewImages ) ? "Yes" : "No") << endl;
-    kdDebug( 51000 ) << "  ImageTitlesWritable: " << (m_interface->hasFeature( KIPI::ImageTitlesWritable ) ? "Yes" : "No") << endl;
-
+    kdDebug( 51000 ) << "Features supported by the host application:" 
+                     << endl;
+    
+    kdDebug( 51000 ) << "  AlbumsHaveComments:  " 
+                     << (m_interface->hasFeature( KIPI::AlbumsHaveComments ) ? "Yes" : "No") 
+                     << endl;
+    
+    kdDebug( 51000 ) << "  AlbumEQDir:          " 
+                     << (m_interface->hasFeature( KIPI::AlbumEQDir ) ? "Yes" : "No") 
+                     << endl;
+    
+    kdDebug( 51000 ) << "  ImagesHasComments:   " 
+                     << (m_interface->hasFeature( KIPI::ImagesHasComments ) ? "Yes" : "No") 
+                     << endl;
+                     
+    kdDebug( 51000 ) << "  ImagesHasTime:       " 
+                     << (m_interface->hasFeature( KIPI::ImagesHasTime ) ? "Yes" : "No") 
+                     << endl;
+                     
+    kdDebug( 51000 ) << "  SupportsDateRanges:  " 
+                     << (m_interface->hasFeature( KIPI::SupportsDateRanges ) ? "Yes" : "No") 
+                     << endl;
+                     
+    kdDebug( 51000 ) << "  AcceptNewImages:     " 
+                     << (m_interface->hasFeature( KIPI::AcceptNewImages ) ? "Yes" : "No") 
+                     << endl;
+                     
+    kdDebug( 51000 ) << "  ImageTitlesWritable: " 
+                     << (m_interface->hasFeature( KIPI::ImageTitlesWritable ) ? "Yes" : "No") 
+                     << endl;
+                     
+    kdDebug( 51000 ) << "  AlbumsHaveCategory: " 
+                     << (m_interface->hasFeature( KIPI::AlbumsHaveCategory ) ? "Yes" : "No") 
+                     << endl;
+                     
+    kdDebug( 51000 ) << "  AlbumsHaveCreationDate: " 
+                     << (m_interface->hasFeature( KIPI::AlbumsHaveCreationDate ) ? "Yes" : "No") 
+                     << endl;
+    
 
     // ================================================== Selection
+    
     kdDebug( 51000 ) << endl
                      << "==================================================" << endl
                      << "                    Selection                     " << endl
                      << "==================================================" << endl;
+    
     KIPI::ImageCollection selection = m_interface->currentSelection();
+    
     if ( !selection.isValid() ) {
         kdDebug( 51000) << "No Selection!" << endl;
     }
     else {
         KURL::List images = selection.images();
+        
         for( KURL::List::Iterator selIt = images.begin(); selIt != images.end(); ++selIt ) {
             kdDebug( 51000 ) <<  *selIt << endl;
             KIPI::ImageInfo info = m_interface->info( *selIt );
@@ -111,21 +147,26 @@ void Plugin_HelloWorld::slotActivate()
     }
 
     // ================================================== Current Album
+    
     kdDebug( 51000 ) << endl
                      << "==================================================" << endl
                      << "                    Current Album                 " << endl
                      << "==================================================" << endl;
+    
     KIPI::ImageCollection album = m_interface->currentAlbum();
+    
     if ( !album.isValid() ) {
         kdDebug( 51000 ) << "No album!" << endl;
     }
     else {
         KURL::List images = album.images();
+        
         for( KURL::List::Iterator albumIt = images.begin(); albumIt != images.end(); ++albumIt ) {
             kdDebug( 51000 ) <<  *albumIt << endl;
         }
 
         kdDebug( 51000 ) << "Album name: " << album.name() << endl;
+        
         if ( m_interface->hasFeature( KIPI::AlbumsHaveComments ) ) {
             kdDebug( 51000 ) << "Album Comment: " << album.comment() << endl;
         }
