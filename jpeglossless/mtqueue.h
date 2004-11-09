63280d436f012ddb4207ddefb6e4b2abd85fe16c
Renchi Raju <renchi.raju@kdemail.net>
2004-11-09 15:43:29 +0000

    added copyright
    svn path=/trunk/kdeextragear-libs-1/kipi-plugins/; revision=361728

--- jpeglossless/mtqueue.h
+++ jpeglossless/mtqueue.h
@@ -1,3 +1,21 @@
+/* ============================================================
+ * File  : mtqueue.h
+ * Author: Renchi Raju <renchi@pooh.tam.uiuc.edu>
+ * Date  : 2004-11-09
+ * Copyright 2004 by Renchi Raju
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ============================================================ */
+
 #ifndef MTQUEUE_H
 #define MTQUEUE_H
 
