bf3fe587d6413090bd48acbfe9131ed2414ffa74
Aurélien Gâteau <agateau@kde.org>
2006-04-18 17:45:42 +0000

    Added missing copyrights, based on Digikam CVS:
    http://cvs.sourceforge.net/viewcvs.py/digikam/digikam3/digikamcameraclient/cameraui/
    
    BUG: 99157
    
    svn path=/trunk/extragear/libs/kipi-plugins/; revision=531203

--- kameraklient/mtqueue.h
+++ kameraklient/mtqueue.h
@@ -1,3 +1,19 @@
+/* ============================================================
+ * Copyright 2003 by Renchi Raju
+ * Copyright 2004 by Tudor Calin
+
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * ============================================================ */
 #ifndef COMMAND_QUEUE_H
 #define COMMAND_QUEUE_H
 
--- kameraklient/thumbitem.cpp
+++ kameraklient/thumbitem.cpp
@@ -1,3 +1,18 @@
+/* ============================================================
+ * Copyright 2004 by Tudor Calin
+
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * ============================================================ */
 // Qt
 #include <qpixmap.h>
 #include <qfont.h>
