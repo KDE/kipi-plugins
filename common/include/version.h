b4e80e2604e5ffc67af39f03e366e39a99b3566b
Gilles Caulier <caulier.gilles@gmail.com>
2006-09-13 21:01:29 +0000

    new version header file
    svn path=/trunk/extragear/libs/kipi-plugins/; revision=583950

---
+++ common/include/version.h
@@ -0,0 +1,28 @@
+/* ============================================================
+ * Authors: Gilles Caulier <caulier dot gilles at free.fr>
+ * Date   : 2006-14-09
+ * Description : Kipi-Plugins release ID header.
+ * 
+ * Copyright 2006 by Gilles Caulier
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * ============================================================ */
+
+
+#ifndef KIPIPLUGINS_VERSION_H
+#define KIPIPLUGINS_VERSION_H
+
+static const char digikamimageplugins_version[] = "0.1.3-svn";
+
+#endif // KIPIPLUGINS_VERSION_H
+
