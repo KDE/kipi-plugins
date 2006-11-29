30245334b196a93b121b81a00d49296ae6e932c3
Angelo Naselli <anaselli@linux.it>
2006-11-29 22:07:57 +0000

    kipi-plugins 0.1.3 beta 1 svn2log.py seems to be broken again used svn2cl instead
    CCMAIL:  kde-imaging@kde.org
    
    svn path=/trunk/extragear/libs/kipi-plugins/; revision=609259

--- common/include/pluginsversion.h
+++ common/include/pluginsversion.h
@@ -21,7 +21,7 @@
 #ifndef KIPIPLUGINS_VERSION_H
 #define KIPIPLUGINS_VERSION_H
 
-static const char kipiplugins_version[] = "0.1.3-svn";
+static const char kipiplugins_version[] = "0.1.3-beta1";
 
 #endif // KIPIPLUGINS_VERSION_H
 
