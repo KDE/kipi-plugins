1618e5e330c49bc5a6ec5719c9c162bfb9dd5a32
Angelo Naselli <anaselli@linux.it>
2007-01-24 20:46:29 +0000

    Kipi-plugins 0.1.3 final
    CCMAIL: <kde-imaging@kde.org>
    
    svn path=/trunk/extragear/libs/kipi-plugins/; revision=626875

--- common/include/pluginsversion.h
+++ common/include/pluginsversion.h
@@ -21,7 +21,7 @@
 #ifndef KIPIPLUGINS_VERSION_H
 #define KIPIPLUGINS_VERSION_H
 
-static const char kipiplugins_version[] = "0.1.3-rc1";
+static const char kipiplugins_version[] = "0.1.3";
 
 #endif // KIPIPLUGINS_VERSION_H
 
