1682670bf29288b0fdfe30c63d556205d6915fab
Gilles Caulier <caulier.gilles@gmail.com>
2007-06-08 14:07:00 +0000

    typo
    svn path=/trunk/extragear/libs/kipi-plugins/; revision=672941

--- cmake/modules/FindKdcraw.cmake
+++ cmake/modules/FindKdcraw.cmake
@@ -20,7 +20,7 @@ else (KDCRAW_INCLUDE_DIR AND KDCRAW_LIBRARIES)
 
   # Check if library is not in local sub-folder
   
-  FIND_FILE(KDCRAW_LOCAL_FOUND libkdraw/version.h ${CMAKE_SOURCE_DIR}/libkdcraw NO_DEFAULT_PATH)
+  FIND_FILE(KDCRAW_LOCAL_FOUND libkdcraw/version.h ${CMAKE_SOURCE_DIR}/libkdcraw NO_DEFAULT_PATH)
 
   if (KDCRAW_LOCAL_FOUND)
 
