19fa05e0cf28f705daa87e887b0866f62d6fab9f
Gilles Caulier <caulier.gilles@gmail.com>
2007-06-08 08:04:18 +0000

    polish
    svn path=/trunk/extragear/libs/kipi-plugins/; revision=672814

--- wallpaper/plugin_wallpaper.cpp
+++ wallpaper/plugin_wallpaper.cpp
@@ -1,66 +1,63 @@
 /* ============================================================
- * File   : plugin_wallpaper.cpp
  *
- * Authors: Gregory KOKANOSKY <gregory dot kokanosky at free.fr>
- *          Gilles Caulier <caulier dot gilles at gmail dot com>
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
  *
- * Date   : 01/2004
+ * Date        : 2004-01-01
+ * Description : Wall Paper kipi-plugin
  *
- * Description : Set Wall paper plugin for KIPI
- *
- * Copyright 2004 by Gregory KOKANOSKY <gregory dot kokanosky at free.fr>
- * Copyright 2004 by Gilles CAULIER <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2004      by Gregory Kokanosky <gregory dot kokanosky at free.fr>
+ * Copyright (C) 2004-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option)
- * any later version.
- *
+ * either version 2, or (at your option) any later version.
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
+ * 
  * ============================================================ */
 
-// Include files for KDE
+// KDE includes
 
- #include <klocale.h>
- #include <kaction.h>
- #include <kgenericfactory.h>
- #include <klibloader.h>
- #include <kconfig.h>
- #include <kdebug.h>
- #include <kdeversion.h>
- #include <krun.h>
- #include <kapplication.h>
- #include <kmessagebox.h>
- #include <ktextbrowser.h>
- #include <kdeversion.h>
- #include <kfiledialog.h>
- #include <kio/netaccess.h>
+#include <klocale.h>
+#include <kaction.h>
+#include <kgenericfactory.h>
+#include <klibloader.h>
+#include <kconfig.h>
+#include <kdebug.h>
+#include <kdeversion.h>
+#include <krun.h>
+#include <kapplication.h>
+#include <kmessagebox.h>
+#include <ktextbrowser.h>
+#include <kdeversion.h>
+#include <kfiledialog.h>
+#include <kio/netaccess.h>
 
 // KIPI includes
 
- #include <libkipi/interface.h>
- #include <libkipi/imagecollection.h>
+#include <libkipi/interface.h>
+#include <libkipi/imagecollection.h>
 
 // Local includes
 
- #include "plugin_wallpaper.h"
+#include "plugin_wallpaper.h"
+#include "plugin_wallpaper.moc"
 
 typedef KGenericFactory<Plugin_WallPaper> Factory;
 
 K_EXPORT_COMPONENT_FACTORY( kipiplugin_wallpaper,
                             Factory("kipiplugin_wallpaper"))
 
- /////////////////////////////////////////////////////////////////////////////////////////////////////
-
- Plugin_WallPaper::Plugin_WallPaper(QObject *parent, const char*, const QStringList&)
-                       : KIPI::Plugin( Factory::instance(), parent, "WallPaper")
- {
-     kdDebug( 51001 ) << "Plugin_WallPaper plugin loaded" << endl;
- }
+Plugin_WallPaper::Plugin_WallPaper(QObject *parent, const char*, const QStringList&)
+                : KIPI::Plugin( Factory::instance(), parent, "WallPaper")
+{
+    kDebug( 51001 ) << "Plugin_WallPaper plugin loaded" << endl;
+}
 
 void Plugin_WallPaper::setup( QWidget* widget )
 {
@@ -119,26 +116,22 @@ void Plugin_WallPaper::setup( QWidget* widget )
                          actionCollection(),
                          "images2desktop_centered_auto_fit"));
 
-    //The Scale & crop code was available from Beta1 on
-    #if KDE_IS_VERSION(3,3,91)
     m_action_Background->insert(new KAction (i18n("Scale && Crop"),
 			 0,
 			 this,
 			 SLOT(slotSetScaleAndCrop()),
 			 actionCollection(),
 			 "images2desktop_scale_and_crop"));
-
-   #endif
     
     addAction( m_action_Background );
 
     KIPI::Interface* interface = dynamic_cast<KIPI::Interface*>( parent() );
 
     if ( !interface )
-           {
-           kdError( 51000 ) << "Kipi interface is null!" << endl;
-           return;
-           }
+    {
+        kError( 51000 ) << "Kipi interface is null!" << endl;
+        return;
+    }
 
     KIPI::ImageCollection selection = interface->currentSelection();
     m_action_Background->setEnabled( selection.isValid() );
@@ -147,70 +140,45 @@ void Plugin_WallPaper::setup( QWidget* widget )
              m_action_Background, SLOT(setEnabled(bool)));
  }
 
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-
 void Plugin_WallPaper::slotSetCenter()
 {
    return setWallpaper(CENTER);
 }
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void Plugin_WallPaper::slotSetTiled()
 {
    return setWallpaper(TILED);
 }
 
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-
 void Plugin_WallPaper::slotSetCenterTiled()
 {
    return  setWallpaper(CENTER_TILED);
 }
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void Plugin_WallPaper::slotSetCenteredMaxpect()
 {
    return setWallpaper(CENTER_MAXPECT);
 }
 
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-
 void Plugin_WallPaper::slotSetTiledMaxpect()
 {
    return setWallpaper(TILED_MAXPECT);
 }
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void Plugin_WallPaper::slotSetScaled()
 {
    return setWallpaper(SCALED);
 }
 
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-
 void Plugin_WallPaper::slotSetCenteredAutoFit()
 {
    return setWallpaper(CENTERED_AUTOFIT);
 }
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-
 void Plugin_WallPaper::slotSetScaleAndCrop()
 {
    return setWallpaper(SCALE_AND_CROP);
 }
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void Plugin_WallPaper::setWallpaper(int layout)
 {
@@ -220,10 +188,10 @@ void Plugin_WallPaper::setWallpaper(int layout)
    KIPI::Interface* interface = dynamic_cast<KIPI::Interface*>( parent() );
 
    if ( !interface )
-           {
-           kdError( 51000 ) << "Kipi interface is null!" << endl;
-           return;
-           }
+   {
+       kError( 51000 ) << "Kipi interface is null!" << endl;
+       return;
+   }
 
    KIPI::ImageCollection images = interface->currentSelection();
 
@@ -232,11 +200,11 @@ void Plugin_WallPaper::setWallpaper(int layout)
    KURL url=images.images()[0];
    QString path;
    if (url.isLocalFile())
-      {
+   {
       path=url.path();
-      }
+   }
    else
-      {
+   {
       // PENDING We need a way to get a parent widget
       // Sun, 06 Jun 2004 - Aur�ien
 
@@ -246,21 +214,14 @@ void Plugin_WallPaper::setWallpaper(int layout)
       path = KFileDialog::getSaveFileName(url.fileName(), QString::null, kapp->activeWindow());
 
       if (path.isNull()) return;
-#if KDE_VERSION > 0x30200
       KIO::NetAccess::download(url, path, 0L);
-#else
-      KIO::NetAccess::download(url, path);
-#endif
-      }
+   }
 
    QString cmd = QString("dcop kdesktop KBackgroundIface setWallpaper '%1' %2")
                          .arg(path).arg(layout);
 
    KRun::runCommand(cmd);
 }
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////
 
 KIPI::Category  Plugin_WallPaper::category( KAction* action ) const
 {
@@ -270,5 +231,3 @@ KIPI::Category  Plugin_WallPaper::category( KAction* action ) const
     kdWarning( 51000 ) << "Unrecognized action for plugin category identification" << endl;
     return KIPI::IMAGESPLUGIN; // no warning from compiler, please
 }
-
-#include "plugin_wallpaper.moc"
--- wallpaper/plugin_wallpaper.h
+++ wallpaper/plugin_wallpaper.h
@@ -1,69 +1,69 @@
 /* ============================================================
- * File   : plugin_wallpaper.h
  *
- * Authors: Gregory KOKANOSKY <gregory dot kokanosky at free.fr>
- *          Gilles Caulier <caulier dot gilles at gmail dot com>
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
  *
- * Date   : 01/2004
+ * Date        : 2004-01-01
+ * Description : Wall Paper kipi-plugin
  *
- * Description : Wall Paper plugin parts for KII
- *
- * Copyright 2004 by Gregory KOKANOSKY <gregory dot kokanosky at free.fr>
- * Copyright 2004 by Gilles CAULIER <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2004      by Gregory Kokanosky <gregory dot kokanosky at free.fr>
+ * Copyright (C) 2004-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option)
- * any later version.
- *
+ * either version 2, or (at your option) any later version.
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * ============================================================ */
 
-  #ifndef PLUGIN_IMAGES2DESKTOP_H
-  #define PLUGIN_IMAGES2DESKTOP_H
+#ifndef PLUGIN_WALLPAPER_H
+#define PLUGIN_WALLPAPER_H
 
-  // Include files for KDE
+// KDE includes
 
-  #include <kprocess.h>
+#include <kprocess.h>
 
-  // KIPI includes
+// KIPI includes
 
-  #include <libkipi/plugin.h>
+#include <libkipi/plugin.h>
 
-  class KActionMenu;
-  class KAction;
+class KActionMenu;
+class KAction;
 
-  class Plugin_WallPaper : public KIPI::Plugin
-  {
-  Q_OBJECT
+class Plugin_WallPaper : public KIPI::Plugin
+{
+Q_OBJECT
 
-   public:
+public:
 
-   Plugin_WallPaper(QObject *parent,
-                      const char* name,
-                      const QStringList &args);
-   virtual KIPI::Category category( KAction* action ) const;
-   virtual void setup( QWidget* );
+    Plugin_WallPaper(QObject *parent, const char* name, const QStringList &args);
+    virtual KIPI::Category category( KAction* action ) const;
+    virtual void setup( QWidget* );
 
-   private slots:
+private slots:
 
-   void slotSetCenter();
-   void slotSetTiled();
-   void slotSetCenterTiled();
-   void slotSetCenteredMaxpect();
-   void slotSetTiledMaxpect();
-   void slotSetScaled();
-   void slotSetCenteredAutoFit();
-   void slotSetScaleAndCrop();
+    void slotSetCenter();
+    void slotSetTiled();
+    void slotSetCenterTiled();
+    void slotSetCenteredMaxpect();
+    void slotSetTiledMaxpect();
+    void slotSetScaled();
+    void slotSetCenteredAutoFit();
+    void slotSetScaleAndCrop();
 
-   private:
+private:
 
-   enum {
+    void setWallpaper( int layout );
+
+private:
+
+    enum 
+    {
         CENTER = 1,
         TILED = 2,
         CENTER_TILED = 3,
@@ -71,11 +71,10 @@
         TILED_MAXPECT = 5,
         SCALED = 6,
         CENTERED_AUTOFIT = 7,
-	SCALE_AND_CROP = 8
-        };
+        SCALE_AND_CROP = 8
+    };
 
-   KActionMenu    *m_action_Background;
-   void setWallpaper( int layout );
-  };
+    KActionMenu *m_action_Background;
+};
 
-  #endif // PLUGIN_IMAGES2DESKTOP_H
+#endif // PLUGIN_WALLPAPER_H
