17b3bd0625cb4578b7a611ce7da5495b79566150
Gilles Caulier <caulier.gilles@gmail.com>
2007-06-09 08:20:23 +0000

    kipi-plugins from KDE3 branch : TimeAdjust : use local date time encoding to display exemple on bottom of dialog BUG: 140890
    svn path=/branches/extragear/kde3/libs/kipi-plugins/; revision=673172

--- timeadjust/timeadjustdialog.cpp
+++ timeadjust/timeadjustdialog.cpp
@@ -1,4 +1,26 @@
-7
+/* ============================================================
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2004-05-16
+ * Description : a plugin to set time stamp of picture files.
+ *
+ * Copyright (C) 2003-2005 by Jesper Pedersen <blackie@kde.org>
+ * Copyright (C) 2006-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * ============================================================ */
+
 // C Ansi includes.
 
 extern "C"
@@ -375,9 +397,9 @@ void TimeAdjustDialog::setImages(const KURL::List& images)
 
 void TimeAdjustDialog::slotUpdateExample()
 {
-    QString oldDate = d->exampleDate.toString();
+    QString oldDate = d->exampleDate.toString(Qt::LocalDate);
     QDateTime date  = updateTime(KURL(), d->exampleDate);
-    QString newDate = date.toString();
+    QString newDate = date.toString(Qt::LocalDate);
     d->exampleAdj->setText(i18n("<b>%1</b><br>would, for example, "
                                "change into<br><b>%2</b>")
                           .arg(oldDate).arg(newDate));
