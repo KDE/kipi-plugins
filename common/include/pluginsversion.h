b674dca2254896f471e08186ff4f06a22ba1996c
Gilles Caulier <caulier.gilles@gmail.com>
2007-06-10 20:47:56 +0000

    fix release id
    svn path=/trunk/extragear/libs/kipi-plugins/; revision=673678

--- common/include/pluginsversion.h
+++ common/include/pluginsversion.h
@@ -1,15 +1,17 @@
 /* ============================================================
- * Authors: Gilles Caulier <caulier dot gilles at free.fr>
- * Date   : 2006-14-09
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2006-14-09
  * Description : Kipi-Plugins release ID header.
  * 
- * Copyright 2006-2007 by Gilles Caulier <caulier dot gilles at free.fr>
+ * Copyright (C) 2006-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option)
- * any later version.
+ * either version 2, or (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -21,7 +23,7 @@
 #ifndef KIPIPLUGINS_VERSION_H
 #define KIPIPLUGINS_VERSION_H
 
-static const char kipiplugins_version[] = "0.1.4-beta1";
+static const char kipiplugins_version[] = "0.2.0";
 
 #endif // KIPIPLUGINS_VERSION_H
 
