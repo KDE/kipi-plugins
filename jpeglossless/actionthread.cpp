30d46cfad73f35a76b9653801cc31cf55346ad96
Gilles Caulier <caulier.gilles@gmail.com>
2007-06-11 06:12:35 +0000

    fix header
    svn path=/branches/extragear/kde3/libs/kipi-plugins/; revision=673850

--- jpeglossless/actionthread.cpp
+++ jpeglossless/actionthread.cpp
@@ -1,24 +1,25 @@
 /* ============================================================
- * Authors     : Renchi Raju 
- *               Gilles Caulier 
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
  * Date        : 2003-12-03
  * Description : a class to manage JPEGLossLess plugin 
  *               actions using threads
  *
- * Copyright 2003-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
- * Copyright 2006-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2003-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
+ * Copyright (C) 2006-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option)
- * any later version.
- *
+ * either version 2, or (at your option) any later version.
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * ============================================================ */
 
 // C Ansi includes.
--- jpeglossless/imageflip.cpp
+++ jpeglossless/imageflip.cpp
@@ -1,21 +1,23 @@
 /* ============================================================
- * Authors     : Gilles Caulier 
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
  * Date        : 2003-10-14
  * Description : batch image flip
  *
- * Copyright 2003-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2003-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option)
- * any later version.
- *
+ * either version 2, or (at your option) any later version.
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * ============================================================ */
 
 #define XMD_H
