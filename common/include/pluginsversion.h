394f5286eb3012533b5e6b5d8cb17c7f7b3ac22a
Valerio Fuoglio <valerio.fuoglio@gmail.com>
2007-06-18 21:49:16 +0000

    Preparing release of kipi-plugins-0.1.4-beta2
    CCMAIL: kde-imaging@kde.org
    
    svn path=/branches/extragear/kde3/libs/kipi-plugins/; revision=677319

--- common/include/pluginsversion.h
+++ common/include/pluginsversion.h
@@ -21,7 +21,7 @@
 #ifndef KIPIPLUGINS_VERSION_H
 #define KIPIPLUGINS_VERSION_H
 
-static const char kipiplugins_version[] = "0.1.4-beta1";
+static const char kipiplugins_version[] = "0.1.4-beta2";
 
 #endif // KIPIPLUGINS_VERSION_H
 
