175495fe65e82749c43e3af3cc14b2b564558cd5
Gilles Caulier <caulier.gilles@gmail.com>
2007-09-25 07:44:45 +0000

    do not use kDebug here too
    svn path=/trunk/extragear/libs/kipi-plugins/; revision=716770

--- jpeglossless/utils.cpp
+++ jpeglossless/utils.cpp
@@ -7,7 +7,12 @@
  * Description : misc utils to used in batch process
  * 
  * Copyright (C) 2003-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
+ * Copyright (C) 2004-2007 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
  * Copyright (C) 2006-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * NOTE: Do not use kdDebug() in this implementation because 
+ *       it will be multithreaded. Use qDebug() instead. 
+ *       See B.K.O #133026 for details.
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -39,10 +44,7 @@ extern "C"
 #include <QString>
 #include <QFile>
 #include <QDir>
-
-// KDE includes.
-
-#include <kdebug.h>
+#include <QtDebug>
 
 // LibKDcraw includes.
 
@@ -111,8 +113,8 @@ bool Utils::MoveFile(const QString& src, const QString& dst)
     struct stat stbuf;
     if (::stat(QFile::encodeName(dst), &stbuf) != 0)
     {
-        kWarning( 51000 ) << "KIPIJPEGLossLessPlugin:MoveFile: failed to stat src"
-                          << endl;
+        qCritical() << "KIPIJPEGLossLessPlugin:MoveFile: failed to stat src"
+                    << endl;
         return false;
     }
 
@@ -124,14 +126,14 @@ bool Utils::MoveFile(const QString& src, const QString& dst)
     timbuf.modtime = stbuf.st_mtime;
     if (::utime(QFile::encodeName(dst), &timbuf) != 0)
     {
-        kWarning( 51000 ) << "KIPIJPEGLossLessPlugin:MoveFile: failed to update dst time"
-                          << endl;
+        qCritical() << "KIPIJPEGLossLessPlugin:MoveFile: failed to update dst time"
+                    << endl;
     }
 
     if (::unlink(QFile::encodeName(src).data()) != 0)
     {
-        kWarning( 51000 ) << "KIPIJPEGLossLessPlugin:MoveFile: failed to unlink src"
-                          << endl;
+        qCritical() << "KIPIJPEGLossLessPlugin:MoveFile: failed to unlink src"
+                    << endl;
     }
     return true;
 }
