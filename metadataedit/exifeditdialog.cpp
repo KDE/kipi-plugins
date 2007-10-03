7578104ebe458963651f89dcc7f771bddb6c4220
Gilles Caulier <caulier.gilles@gmail.com>
2007-10-03 12:00:33 +0000

    fix header
    svn path=/branches/extragear/kde3/libs/kipi-plugins/; revision=720647

--- metadataedit/exifeditdialog.cpp
+++ metadataedit/exifeditdialog.cpp
@@ -1,21 +1,23 @@
 /* ============================================================
- * Authors     : Caulier Gilles 
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
  * Date        : 2006-10-12
  * Description : a dialog to edit EXIF metadata
- * 
- * Copyright 2006-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * Copyright (C) 2006-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option)
- * any later version.
+ * either version 2, or (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * ============================================================ */
 
 // Qt includes.
