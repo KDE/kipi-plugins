57ef98c37cdb8966deb90f77ac70cf0447b4a7c6
Gilles Caulier <caulier.gilles@gmail.com>
2007-10-24 12:30:13 +0000

    polish
    svn path=/trunk/extragear/libs/kipi-plugins/; revision=728839

--- metadataedit/objectattributesedit.h
+++ metadataedit/objectattributesedit.h
@@ -39,7 +39,7 @@ class ObjectAttributesEdit : public QWidget
     
 public:
 
-    ObjectAttributesEdit(QWidget* parent, bool ascii, int size);
+    ObjectAttributesEdit(QWidget* parent, bool ascii=true, int size=-1);
     ~ObjectAttributesEdit();
 
     void setValid(bool v);
