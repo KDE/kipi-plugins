d05f322253a3c2a6ae5c888fc64050608a054696
Gilles Caulier <caulier.gilles@gmail.com>
2007-10-28 10:58:24 +0000

    fix email
    svn path=/branches/extragear/kde3/libs/kipi-plugins/; revision=730283

--- sendimages/sendimagesdialog.cpp
+++ sendimages/sendimagesdialog.cpp
@@ -8,6 +8,7 @@
  *
  * Copyright (C) 2003-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2006 by Tom Albers <tomalbers at kde dot nl>
+ * Copyright (C) 2006 by Michael Höchstetter <michael dot hoechstetter at gmx dot de>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -164,7 +165,7 @@ SendImagesDialog::SendImagesDialog(QWidget *parent, KIPI::Interface* interface,
                        "caulier dot gilles at gmail dot com");
 
     m_about->addAuthor("Michael Höchstetter", I18N_NOOP("Developer"),
-                       "michae dot hoechstetter at gmx dot de");
+                       "michael dot hoechstetter at gmx dot de");
 
     m_about->addAuthor("Tom Albers", I18N_NOOP("Developer"),
                        "tomalbers at kde dot nl");
