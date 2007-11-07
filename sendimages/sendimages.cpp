ebbd9fa3fd4acdebce55daa2a0a95b53016152cc
Gilles Caulier <caulier.gilles@gmail.com>
2007-11-07 13:05:02 +0000

    do not use UF8 encoding in source code
    svn path=/branches/extragear/kde3/libs/kipi-plugins/; revision=733882

--- sendimages/sendimages.cpp
+++ sendimages/sendimages.cpp
@@ -8,7 +8,7 @@
  *
  * Copyright (C) 2004-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2006 by Tom Albers <tomalbers at kde dot nl>
- * Copyright (C) 2006 by Michael Höchstetter <michael dot hoechstetter at gmx dot de>
+ * Copyright (C) 2006 by Michael Hoechstetter <michael dot hoechstetter at gmx dot de>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
--- sendimages/sendimagesdialog.cpp
+++ sendimages/sendimagesdialog.cpp
@@ -8,7 +8,7 @@
  *
  * Copyright (C) 2003-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2006 by Tom Albers <tomalbers at kde dot nl>
- * Copyright (C) 2006 by Michael Höchstetter <michael dot hoechstetter at gmx dot de>
+ * Copyright (C) 2006 by Michael Hoechstetter <michael dot hoechstetter at gmx dot de>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -164,7 +164,7 @@ SendImagesDialog::SendImagesDialog(QWidget *parent, KIPI::Interface* interface,
     m_about->addAuthor("Gilles Caulier", I18N_NOOP("Author and maintainer"),
                        "caulier dot gilles at gmail dot com");
 
-    m_about->addAuthor("Michael Höchstetter", I18N_NOOP("Developer"),
+    m_about->addAuthor("Michael Hoechstetter", I18N_NOOP("Developer"),
                        "michael dot hoechstetter at gmx dot de");
 
     m_about->addAuthor("Tom Albers", I18N_NOOP("Developer"),
