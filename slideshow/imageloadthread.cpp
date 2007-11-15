68b97d63ff00db179f44e23e160622701e4d33a4
Gilles Caulier <caulier.gilles@gmail.com>
2007-11-15 13:53:11 +0000

    Code polishing :
    - Header revamping following tip from Achim.
    - Always put the .moc includes with the list on local includes files. This must be the last one.
    - In header, place declaration in right order. It's more readable : public, signals, protected, private.
    
    Valerio, when you can, i recommend you to use a "d" private class everywhere to host all private member. This will speedup compilation and reduce 
    depencies between other sources file (in a library, this way improve binary compatibility)
    
    CCMAIL: valerio.fuoglio@gmail.com
    
    
    svn path=/branches/extragear/kde3/libs/kipi-plugins/; revision=737098

--- slideshow/imageloadthread.cpp
+++ slideshow/imageloadthread.cpp
@@ -1,28 +1,29 @@
 /* ============================================================
- * File  : imageloadthread.cpp
- * Author: Valerio Fuoglio <valerio.fuoglio@gmail.com>
- * Date  : 2007-11-14
- * Description : 
- * 
- * Copyright 2007 by Valerio Fuoglio <valerio.fuoglio@gmail.com>
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2007-11-14
+ * Description : a kipi plugin to slide images.
+ *
+ * Copyright (C) 2007 by Valerio Fuoglio <valerio dot fuoglio at gmail dot com>
+ *
+ * Parts of this code are based on smoothslidesaver by Carsten Weinhold 
+ * <carsten dot weinhold at gmx dot de>                                           
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option)
- * any later version.
- * 
+ * either version 2, or (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * ============================================================ */
 
-/***************************************************************************
- *   Parts of this code are based on smoothslidesaver by Carsten Weinhold  *
- *   <carsten.weinhold@gmx.de>                                             *
- **************************************************************************/
+// Qt includes.
 
 #include <qdir.h>
 #include <qvaluevector.h>
@@ -30,17 +31,21 @@
 #include <qwmatrix.h>
 #include <qobject.h>
              
+// KDE includes.
+
 #include <klocale.h>
              
+// Local includes.
+
 #include "slideshowkb.h"
 #include "imageloadthread.h"
-
-
+#include "imageloadthread.moc"
 
 namespace KIPISlideShowPlugin
 {
-  ImageLoadThread::ImageLoadThread(QValueList<QPair<QString, int> >& fileList, 
-                                  int width, int height) 
+
+ImageLoadThread::ImageLoadThread(QValueList<QPair<QString, int> >& fileList, 
+                                 int width, int height) 
 {
 
     m_initialized   = false;
@@ -178,5 +183,3 @@ void ImageLoadThread::invalidateCurrentImageName() {
 }
 
 }  // NameSpace KIPISlideShowPlugin
-
-#include "imageloadthread.moc"
--- slideshow/imageloadthread.h
+++ slideshow/imageloadthread.h
@@ -1,31 +1,32 @@
 /* ============================================================
- * File  : imageloadthread.h
- * Author: Valerio Fuoglio <valerio.fuoglio@gmail.com>
- * Date  : 2007-11-14
- * Description : 
- * 
- * Copyright 2007 by Valerio Fuoglio <valerio.fuoglio@gmail.com>
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2007-11-14
+ * Description : a kipi plugin to slide images.
+ *
+ * Copyright (C) 2007 by Valerio Fuoglio <valerio dot fuoglio at gmail dot com>
+ *
+ * Parts of this code are based on smoothslidesaver by Carsten Weinhold 
+ * <carsten dot weinhold at gmx dot de>                                           
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option)
- * any later version.
- * 
+ * either version 2, or (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * ============================================================ */
-
-/***************************************************************************
- *   Parts of this code are based on smoothslidesaver by Carsten Weinhold  *
- *   <carsten.weinhold@gmx.de>                                             *
- **************************************************************************/
 
 #ifndef IMAGELOADTHREAD_H
 #define IMAGELOADTHREAD_H
+
+// QT includes.
 
 #include <qimage.h>
 #include <qthread.h>
@@ -38,43 +39,50 @@
 
 namespace KIPISlideShowPlugin
 {
+
 class ImageLoadThread : public QObject, public QThread
 {
+
 Q_OBJECT
+
 public:
+
   ImageLoadThread(QValueList<QPair<QString, int> >& fileList, int width, int height);
     
-    void          quit();
-    void          requestNewImage();
-    bool          grabImage()   { m_imageLock.lock(); return m_haveImages; };
-    void          ungrabImage() { m_imageLock.unlock(); };
-    bool          ready()       { return m_initialized; };
+    void  quit();
+    void  requestNewImage();
+    bool  grabImage()   { m_imageLock.lock(); return m_haveImages; };
+    void  ungrabImage() { m_imageLock.unlock(); };
+    bool  ready()       { return m_initialized; };
     const QImage &image()       { return m_texture; };
-    float         imageAspect() { return m_textureAspect; };
+    float imageAspect() { return m_textureAspect; };
+
+signals:
+
+    void endOfShow();
 
 protected:
+
     void run();
 
     bool loadImage();
-    void        invalidateCurrentImageName();
+    void invalidateCurrentImageName();
     
 private:
   
-    int            m_fileIndex;
+    int                               m_fileIndex;
     QValueList<QPair<QString, int> >  m_fileList;
     
-    int m_width, m_height;
+    int                               m_width, m_height;
     
-    QWaitCondition m_imageRequest;
-    QMutex         m_condLock, m_imageLock;
-    bool           m_initialized, m_needImage, m_haveImages, m_quitRequested, m_scanSubdirectories;
+    QWaitCondition                    m_imageRequest;
+    QMutex                            m_condLock, m_imageLock;
+    bool                              m_initialized, m_needImage, m_haveImages, m_quitRequested, m_scanSubdirectories;
     
-    float          m_textureAspect;
-    QImage         m_texture;
-    
-  signals:
-    void endOfShow();
+    float                             m_textureAspect;
+    QImage                            m_texture;
 };
 
 }  // NameSpace KIPISlideShowPlugin
-#endif
+
+#endif // IMAGELOADTHREAD_H
