5b7f372dbff0d06c82caa99d9894a93d1f46f0e8
Gilles Caulier <caulier.gilles@gmail.com>
2007-11-15 14:08:34 +0000

    polish
    svn path=/branches/extragear/kde3/libs/kipi-plugins/; revision=737104

--- slideshow/listimageitems.h
+++ slideshow/listimageitems.h
@@ -1,18 +1,21 @@
 /* ============================================================
- * File  : listimageitems.h
- * Author: Gilles Caulier <caulier dot gilles at gmail dot com>
  *
- * Copyright (C) 2003 Gilles Caulier <caulier dot gilles at gmail dot com>
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2003-10-21
+ * Description : a kipi plugin to slide images.
+ *
+ * Copyright (C) 2003-2007 Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option)
- * any later version.
+ * either version 2, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * ============================================================ */
@@ -30,7 +33,6 @@
 
 #include <klistbox.h>
 #include <kurl.h>
-
 
 namespace KIPISlideShowPlugin
 {
