821884cad980d3302928ae809f424701bb35949c
Gilles Caulier <caulier.gilles@gmail.com>
2007-11-15 13:58:47 +0000

    polish
    svn path=/branches/extragear/kde3/libs/kipi-plugins/; revision=737101

--- slideshow/kbeffect.cpp
+++ slideshow/kbeffect.cpp
@@ -1,32 +1,42 @@
 /* ============================================================
- * File  : kbeffect.cpp
- * Author: Valerio Fuoglio <valerio.fuoglio@gmail.com>
- * Date  : 2007-11-14
- * Description : 
- * 
- * Copyright 2007 by Valerio Fuoglio <valerio.fuoglio@gmail.com>
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2007-11-14
+ * Description : a kipi plugin to slide images.
+ *
+ * Copyright (C) 2007 by Valerio Fuoglio <valerio dot fuoglio at gmail dot com>
+ *
+ * Parts of this code are based on smoothslidesaver by Carsten Weinhold 
+ * <carsten dot weinhold at gmx dot de>                                           
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option)
- * any later version.
- * 
+ * either version 2, or (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * ============================================================ */
 
-/***************************************************************************
- *   Parts of this code are based on smoothslidesaver by Carsten Weinhold  *
- *   <carsten.weinhold@gmx.de>                                             *
- **************************************************************************/
+// C++ includes.
 
 #include <assert.h>
+
+// Qt includes.
+
 #include <qdatetime.h>
+
+// KDE includes.
+
 #include "kbeffect.h"
+
+// Local includes.
+
 #include "slideshowkb.h"
 
 namespace KIPISlideShowPlugin
@@ -178,6 +188,5 @@ void BlendKBEffect::advanceTime(float step) {
     } else
         m_img[0]->m_opacity = 1.0;
 }
-
 
 }  // NameSpace KIPISlideShowPlugin
--- slideshow/kbeffect.h
+++ slideshow/kbeffect.h
@@ -1,38 +1,42 @@
 /* ============================================================
- * File  : kbeffect.h
- * Author: Valerio Fuoglio <valerio.fuoglio@gmail.com>
- * Date  : 2007-11-14
- * Description : 
- * 
- * Copyright 2007 by Valerio Fuoglio <valerio.fuoglio@gmail.com>
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2007-11-14
+ * Description : a kipi plugin to slide images.
+ *
+ * Copyright (C) 2007 by Valerio Fuoglio <valerio dot fuoglio at gmail dot com>
+ *
+ * Parts of this code are based on smoothslidesaver by Carsten Weinhold 
+ * <carsten dot weinhold at gmx dot de>                                           
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option)
- * any later version.
- * 
+ * either version 2, or (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * ============================================================ */
-
-/***************************************************************************
- *   Parts of this code are based on smoothslidesaver by Carsten Weinhold  *
- *   <carsten.weinhold@gmx.de>                                             *
- **************************************************************************/
 
 #ifndef KBEFFECT_H
 #define KBEFFECT_H
+
 namespace KIPISlideShowPlugin
 {
+
 class Image;
 class SlideShowKB;
 
-class KBEffect {
+class KBEffect 
+{
+
 public:
+
     typedef enum { Fade, Blend } Type;
     
     KBEffect(SlideShowKB *parent, bool m_needFadeIn = true);
@@ -46,23 +50,29 @@ public:
     static Type chooseKBEffect(Type oldType);
 
 protected:
+
     void   setupNewImage(int img);
     void   swapImages();
     Image *image(int img);
             
-private:
-    SlideShowKB *slideWidget;
-
 protected:
+
     static int m_numKBEffectRepeated;
     bool       m_needFadeIn;
     Image     *m_img[2];
+
+private:
+
+    SlideShowKB *slideWidget;
 };
 
 // -------------------------------------------------------------------------
 
-class FadeKBEffect: public KBEffect {
+class FadeKBEffect: public KBEffect 
+{
+
 public:
+
     FadeKBEffect(SlideShowKB *parent, bool m_needFadeIn = true);
     virtual ~FadeKBEffect();
 
@@ -73,8 +83,11 @@ public:
 
 // -------------------------------------------------------------------------
 
-class BlendKBEffect: public KBEffect {
+class BlendKBEffect: public KBEffect 
+{
+
 public:
+
     BlendKBEffect(SlideShowKB *parent, bool m_needFadeIn = true);
     virtual ~BlendKBEffect();
 
@@ -84,5 +97,5 @@ public:
 };
 
 }  // NameSpace KIPISlideShowPlugin
-#endif
 
+#endif // KBEFFECT_H
