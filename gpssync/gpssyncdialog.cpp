c887f1f7c42166ccf93cc1f1cfb3e6bdf0866157
Gilles Caulier <caulier.gilles@gmail.com>
2007-12-11 13:18:32 +0000

    polish
    svn path=/branches/extragear/kde3/libs/kipi-plugins/; revision=747251

--- gpssync/gpssyncdialog.cpp
+++ gpssync/gpssyncdialog.cpp
@@ -156,7 +156,7 @@ GPSSyncDialog::GPSSyncDialog( KIPI::Interface* interface, QWidget* parent)
     KSeparator *line     = new KSeparator(Horizontal, settingsBox);
 
     QLabel *maxGapLabel = new QLabel(i18n("Max. time gap (sec.):"), settingsBox);
-    d->maxGapInput      = new KIntSpinBox(0, 100000, 1, 30, 10, settingsBox);
+    d->maxGapInput      = new KIntSpinBox(0, 1000000, 1, 30, 10, settingsBox);
     QWhatsThis::add(d->maxGapInput, i18n("<p>Sets the maximum difference in "
                     "seconds from a GPS track point to the image time to be matched. "
                     "If the time difference exceeds this setting, no match takes place."));
