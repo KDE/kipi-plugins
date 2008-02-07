6d1bca913cc52069f2fbafa1b50b3ed89da129fd
Gilles Caulier <caulier.gilles@gmail.com>
2008-02-07 10:01:31 +0000

    polish
    svn path=/trunk/extragear/libs/kipi-plugins/; revision=771951

--- gpssync/gpseditdialog.cpp
+++ gpssync/gpseditdialog.cpp
@@ -224,6 +224,7 @@ void GPSEditDialog::slotGotoLocation()
 {
     if (!checkGPSLocation()) return;
     d->worldMap->setGPSPosition(d->latitudeInput->text(), d->longitudeInput->text());
+    slotUpdateWorldMap();
 }
 
 void GPSEditDialog::slotUpdateWorldMap()
