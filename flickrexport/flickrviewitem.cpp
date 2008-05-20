028477d07da5ab2010aed047bf1a24606d01c940
Gilles Caulier <caulier.gilles@gmail.com>
2008-05-20 06:38:36 +0000

    fix coding style
    svn path=/branches/extragear/kde3/libs/kipi-plugins/; revision=810154

--- flickrexport/flickrviewitem.cpp
+++ flickrexport/flickrviewitem.cpp
@@ -1,26 +1,32 @@
 /* ============================================================
- * File  : flickrviewitem.cpp
- * Author: Renchi Raju <renchi@pooh.tam.uiuc.edu>
- * Date  : 2004-12-03
- * Description : 
- * 
- * Copyright 2004 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
-
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2004-12-01
+ * Description : a kipi plugin to export images to Flickr web service
+ *
+ * Copyright (C) 2004 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
+ * Copyright (C) 2005-2008 by Vardhman Jain <vardhman at gmail dot com>
+ *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option)
- * any later version.
- * 
+ * either version 2, or (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * ============================================================ */
+
+// Qt includes.
 
 #include <qpainter.h>
 #include <qpixmap.h>
+
+// Local includes.
 
 #include "flickrviewitem.h"
 
@@ -36,14 +42,15 @@ void GAlbumViewItem::paintCell(QPainter * p, const QColorGroup & cg,
     QListView *lv = listView();
     if (!lv)
         return;
+
     QFontMetrics fm(p->fontMetrics());
 
     if (isSelected())
         p->fillRect(0, 0, width, height(), cg.highlight());
     else
         p->fillRect(0, 0, width, height(), cg.base());
-        
-    const QPixmap * icon = pixmap( column );
+
+    const QPixmap* icon = pixmap(column);
 
     int iconWidth = 0;
     if (icon)
@@ -73,9 +80,9 @@ void GAlbumViewItem::paintCell(QPainter * p, const QColorGroup & cg,
 
 void GAlbumViewItem::setup()
 {
-    int h = listView()->fontMetrics().height();
+    int h      = listView()->fontMetrics().height();
     int margin = 4;
     setHeight( QMAX(2*h + margin, 32) );
 }
 
-}
+} // namespace KIPIFlickrExportPlugin
--- flickrexport/flickrviewitem.h
+++ flickrexport/flickrviewitem.h
@@ -1,25 +1,34 @@
 /* ============================================================
- * File  : flickrviewitem.h
- * Author: Renchi Raju <renchi@pooh.tam.uiuc.edu>
- * Date  : 2004-12-01
- * Copyright 2004 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2004-12-01
+ * Description : a kipi plugin to export images to Flickr web service
+ *
+ * Copyright (C) 2004 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
+ * Copyright (C) 2005-2008 by Vardhman Jain <vardhman at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option)
- * any later version.
- * 
+ * either version 2, or (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
+ *
  * ============================================================ */
 
 #ifndef FLICKRVIEWITEM_H
 #define FLICKRVIEWITEM_H
 
+// Qt includes.
+
 #include <qlistview.h>
+
+// Local includes.
 
 #include "flickritem.h"
 
@@ -28,28 +37,27 @@ namespace KIPIFlickrExportPlugin
 
 class GAlbumViewItem : public QListViewItem
 {
+
 public:
 
-    GAlbumViewItem(QListView* parent, const QString& name,
-                   const GAlbum& _album)
+    GAlbumViewItem(QListView* parent, const QString& name, const GAlbum& _album)
         : QListViewItem(parent, name), album(_album) {}
-    GAlbumViewItem(QListViewItem* parent, const QString& name,
-                   const GAlbum& _album)
+
+    GAlbumViewItem(QListViewItem* parent, const QString& name, const GAlbum& _album)
         : QListViewItem(parent, name), album(_album) {}
+
+    void paintCell(QPainter *p, const QColorGroup& cg, int column, int width, int);
+    void paintFocus (QPainter*, const QColorGroup&, const QRect&) {}
+
+public:
 
     GAlbum album;
-
-    void paintCell(QPainter * p, const QColorGroup & cg,
-                   int column, int width, int );
-    void paintFocus (QPainter*, const QColorGroup&,
-                     const QRect&) {}
 
 protected:
 
     void setup();
-    
 };
 
-}
+} // namespace KIPIFlickrExportPlugin
 
 #endif /* FLICKRVIEWITEM_H */
