ece2aa8ea1bd93806818ea3cf27e5ab7a2efb419
Gilles Caulier <caulier.gilles@gmail.com>
2008-05-20 05:54:11 +0000

    fix coding style
    svn path=/branches/extragear/kde3/libs/kipi-plugins/; revision=810146

--- flickrexport/flickritem.h
+++ flickrexport/flickritem.h
@@ -1,26 +1,29 @@
 /* ============================================================
- * File  : flickritem.h
- * Author: Vardhman Jain <vardhman @ gmail.com>
- * Date  : 2005-07-07
- * Description : 
- * 
- * Copyright 2005 By Vardhman Jain <vardhman @ gmail.com>
-
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2005-07-07
+ * Description : a kipi plugin to export images to Flickr web service
+ *
+ * Copyright (C) 2005-2008 by Vardhman Jain <vardhman at gmail dot com>
+ *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
- * Public License as published bythe Free Software Foundation;
- * either version 2, or (at your option)
- * any later version.
- * 
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * ============================================================ */
 
 #ifndef FLICKRITEM_H
 #define FLICKRITEM_H
+
+// Qt includes.
 
 #include <qstring.h>
 
@@ -29,49 +32,58 @@ namespace KIPIFlickrExportPlugin
 
 class GPhoto
 {
+
 public:
 
     GPhoto()
     {
-        //ref_num = -1;
+//      ref_num = -1;
     }
-    
-    int           ref_num;
-    //int           album_num;
-    QStrList      tags;
-    QString       title;
-    QString       description;
-    bool 	 is_public;
-    bool 	 is_private;
-    bool 	 is_family;
-    
-  //  QString       caption;
-   // QString       thumbName;
-   // QString       albumURL;
-};    
+
+    bool     is_public;
+    bool     is_private;
+    bool     is_family;
+
+    int      ref_num;
+//  int      album_num;
+
+    QStrList tags;
+    QString  title;
+    QString  description;
+
+//  QString  caption;
+//  QString  thumbName;
+//  QString  albumURL;
+};
+
+// -------------------------------------------------------------
 
 class FPhotoInfo
 {
+
 public:
 
     FPhotoInfo()
     {
-    is_public=false;
-    is_family=false;
-    is_friend=false;
+        is_public = false;
+        is_family = false;
+        is_friend = false;
     }
-    
-    
-    QString       title;
-    QString       description;
-    QStringList      tags;
-    bool 	 is_public;
-    bool 	 is_friend;
-    bool 	 is_family;
+
+    bool        is_public;
+    bool        is_friend;
+    bool        is_family;
+
+    QString     title;
+    QString     description;
+    QStringList tags;
 };
+
+// -------------------------------------------------------------
 
 class GAlbum
 {
+
 public:
 
     GAlbum()
@@ -86,35 +98,43 @@ public:
         create_sub     = false;
     }
 
-    int                ref_num;
-    int                parent_ref_num;
-    QString            name;
-    QString            parentName;
-    QString            title;
-    QString            summary;
-    QString            baseurl;
-                       
-    bool               add;
-    bool               write;
-    bool               del_item;
-    bool               del_alb;
-    bool               create_sub;
+    bool    add;
+    bool    write;
+    bool    del_item;
+    bool    del_alb;
+    bool    create_sub;
+
+    int     ref_num;
+    int     parent_ref_num;
+
+    QString name;
+    QString parentName;
+    QString title;
+    QString summary;
+    QString baseurl;
 };
+
+// -------------------------------------------------------------
 
 class FPhotoSet
 {
-	public:
-		FPhotoSet()
-		{
-			id="-1";
-		}
-	QString  id;
-	QString primary; //="2483" 
-	QString secret;//="abcdef"
-	QString server;
-	QString photos;
-	QString title;
-	QString description;
+
+public:
+
+    FPhotoSet()
+    {
+        id = "-1";
+    }
+
+    QString id;
+    QString primary;    //="2483" 
+    QString secret;     //="abcdef"
+    QString server;
+    QString photos;
+    QString title;
+    QString description;
 };
-}
+
+} // namespace KIPIFlickrExportPlugin
+
 #endif /* FLICKRITEM_H */
