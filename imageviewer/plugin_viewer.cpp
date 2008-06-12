b9fa686d70b101f7a2eb41d5c5025ef93373b264
Gilles Caulier <caulier.gilles@gmail.com>
2008-06-12 09:58:39 +0000

    fix header acordingly with Achim bohnet tip from Debian. missing moc file remove tabs in source code. always use spaces instead
    svn path=/trunk/extragear/graphics/kipi-plugins/; revision=819835

--- imageviewer/plugin_viewer.cpp
+++ imageviewer/plugin_viewer.cpp
@@ -1,82 +1,93 @@
-/***************************************************************************
- *   Copyright (C) 2007-2008 by Markus Leuthold   *
- *   <kusi (+at) forum.titlis.org>   *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA.        *
- ***************************************************************************/
+/* ============================================================
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2007-02-11
+ * Description : a kipi plugin to show image using 
+ *               an OpenGL interface.
+ *
+ * Copyright (C) 2007-2008 by Markus Leuthold <kusi at forum dot titlis dot  org>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+// QT includes
+
+#include <qmessagebox.h>
 
 // KDE includes
+
 #include <kaction.h>
 #include <kactioncollection.h>
 #include <kgenericfactory.h>
 #include <kmessagebox.h>
 #include <kurl.h>
 
-// QT includes
-#include <qmessagebox.h>
-
 // kipi includes
+
 #include <libkipi/imageinfo.h>
 //TODO new:necessary
 #include <libkipi/interface.h>
 #include <libkipi/imagecollection.h>
 
 // local includes
-#include "plugin_viewer.h"
+
 #include "viewerwidget.h"
+#include "plugin_viewer.h"
+#include "plugin_viewer.moc"
 
 K_PLUGIN_FACTORY( viewerFactory, registerPlugin<Plugin_viewer>(); )
 K_EXPORT_PLUGIN ( viewerFactory("kipiplugin_imageviewer") )
 
 Plugin_viewer::Plugin_viewer( QObject *parent, const QVariantList & )
-    :KIPI::Plugin::Plugin( viewerFactory::componentData(), parent, "OpenGL image viewer" )
+             : KIPI::Plugin::Plugin( viewerFactory::componentData(), parent, "kipiplugin_imageviewer" )
 {
-	kDebug() << "OpenGL viewer plugin loaded";
+    kDebug() << "OpenGL viewer plugin loaded";
 }
 
 void Plugin_viewer::setup( QWidget* widget )
 {
-	KIPI::Plugin::setup( widget );
-	
-	KIPI::Interface* interface = dynamic_cast<KIPI::Interface*>( parent() );
-	
-	if ( !interface ) 
-	{
-		kError() << "Kipi interface is null!" << endl;
-		return;
-	}
-	
-	actionViewer = new KAction(i18n("OpenGL Image Viewer"), actionCollection());
-	actionViewer->setObjectName("oglviewer");
-	connect(actionViewer, SIGNAL(triggered(bool)),
-		this, SLOT(slotActivate()));
-	addAction(actionViewer);
-	
-	widget=0;
+    KIPI::Plugin::setup( widget );
+
+    KIPI::Interface* interface = dynamic_cast<KIPI::Interface*>( parent() );
+
+    if ( !interface ) 
+    {
+        kError() << "Kipi interface is null!" << endl;
+        return;
+    }
+
+    actionViewer = new KAction(i18n("OpenGL Image Viewer"), actionCollection());
+    actionViewer->setObjectName("oglviewer");
+    connect(actionViewer, SIGNAL(triggered(bool)),
+            this, SLOT(slotActivate()));
+
+    addAction(actionViewer);
+
+    widget=0;
 }
 
 KIPI::Category Plugin_viewer::category( KAction* action ) const
 {
-	if ( action == actionViewer ) {
-		return KIPI::ToolsPlugin;
-	}
-	else {
-		kWarning() << "Unrecognized action for plugin category identification" << endl;
-		return KIPI::ToolsPlugin; // no warning from compiler, please
-	}
+    if ( action == actionViewer ) 
+    {
+        return KIPI::ToolsPlugin;
+    }
+    else 
+    {
+        kWarning() << "Unrecognized action for plugin category identification" << endl;
+        return KIPI::ToolsPlugin; // no warning from compiler, please
+    }
 }
 
 
@@ -85,30 +96,31 @@ KIPI::Category Plugin_viewer::category( KAction* action ) const
  */
 void  Plugin_viewer::slotActivate()
 {
-	KIPI::Interface* interface = dynamic_cast<KIPI::Interface*>( parent() );
-	
-	if ( !interface ) 
-	{
-		kError() << "Kipi interface is null!" << endl;
-		return;
-	}
-	
-	widget = new KIPIviewer::ViewerWidget(interface);
-	
-	switch(widget->getOGLstate()) {
-		case KIPIviewer::oglOK:
-			widget->show();
-			break;
-			
-		case KIPIviewer::oglNoRectangularTexture:
-			kError() << "GL_ARB_texture_rectangle not supported" << endl;
-			delete widget;
-			QMessageBox::critical(new QWidget(),"OpenGL error","GL_ARB_texture_rectangle not supported");
-			break;
-			
-		case KIPIviewer::oglNoContext:
-			kError() << "no OpenGL context found" << endl;
-			delete widget;
-			QMessageBox::critical(new QWidget(),"OpenGL error","no OpenGL context found");
-	}
+    KIPI::Interface* interface = dynamic_cast<KIPI::Interface*>( parent() );
+
+    if ( !interface ) 
+    {
+        kError() << "Kipi interface is null!" << endl;
+        return;
+    }
+
+    widget = new KIPIviewer::ViewerWidget(interface);
+
+    switch(widget->getOGLstate()) 
+    {
+        case KIPIviewer::oglOK:
+            widget->show();
+            break;
+
+        case KIPIviewer::oglNoRectangularTexture:
+            kError() << "GL_ARB_texture_rectangle not supported" << endl;
+            delete widget;
+            QMessageBox::critical(new QWidget(),"OpenGL error","GL_ARB_texture_rectangle not supported");
+            break;
+
+        case KIPIviewer::oglNoContext:
+            kError() << "no OpenGL context found" << endl;
+            delete widget;
+            QMessageBox::critical(new QWidget(),"OpenGL error","no OpenGL context found");
+    }
 }
--- imageviewer/plugin_viewer.h
+++ imageviewer/plugin_viewer.h
@@ -1,31 +1,36 @@
-/***************************************************************************
- *   Copyright (C) 2007 by Markus Leuthold   *
- *   <kusi (+at) forum.titlis.org>   *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA.        *
- ***************************************************************************/
+/* ============================================================
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2007-02-11
+ * Description : a kipi plugin to show image using 
+ *               an OpenGL interface.
+ *
+ * Copyright (C) 2007-2008 by Markus Leuthold <kusi at forum dot titlis dot  org>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
 #ifndef PLUGIN_VIEWER_H
 #define PLUGIN_VIEWER_H
 
 // kipi includes
+
 #include <libkipi/plugin.h>
 
 // local includes
-#include "viewerwidget.h"
 
+#include "viewerwidget.h"
 
 class KAction;
 
@@ -34,22 +39,24 @@ class KAction;
  * @author Markus Leuthold <kusi (+at) forum.titlis.org>
  * @version 0.2
  */
-
 class Plugin_viewer :public KIPI::Plugin
 {
-	Q_OBJECT
+    Q_OBJECT
+
 public:
-	Plugin_viewer( QObject *parent, const QVariantList &  );
-	virtual void setup( QWidget* widget );
-	virtual KIPI::Category category( KAction* action ) const;
+
+    Plugin_viewer( QObject *parent, const QVariantList &  );
+    virtual void setup( QWidget* widget );
+    virtual KIPI::Category category( KAction* action ) const;
 
 protected:
-	KIPIviewer::ViewerWidget * widget;
-	KAction * actionViewer;
-	
+
+    KIPIviewer::ViewerWidget * widget;
+    KAction * actionViewer;
+
 public slots:
-	void  slotActivate();
+
+    void  slotActivate();
 };
 
 #endif /* PLUGIN_VIEWER_H */
-
