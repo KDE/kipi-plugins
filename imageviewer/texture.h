b355285bad7cbd736aebe4f6c37329ef4093250b
Gilles Caulier <caulier.gilles@gmail.com>
2008-06-12 10:03:53 +0000

    fix header remove tab polish
    svn path=/trunk/extragear/graphics/kipi-plugins/; revision=819837

--- imageviewer/texture.h
+++ imageviewer/texture.h
@@ -1,22 +1,25 @@
-/***************************************************************************
- *   Copyright (C) 2007 by Markus Leuthold   *
- *   <kusi (+at) forum.titlis.org>   *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA.        *
- ***************************************************************************/
+/* ============================================================
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2007-02-11
+ * Description : a kipi plugin to show image using 
+ *               an OpenGL interface.
+ *
+ * Copyright (C) 2007-2008 by Markus Leuthold <kusi at forum dot titlis dot org>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
 
 #ifndef TEXTURE_H
 #define TEXTURE_H
@@ -33,42 +36,50 @@
  * @version 0.2
  */
 
-namespace KIPIviewer {
-class Texture{
-public:
-	Texture();
-	~Texture();
-	int height();
-	int width();
-	bool load(QString fn, QSize size, GLuint tn);
-	bool load(QImage im, QSize size, GLuint tn);
-	GLvoid * data();
-	GLuint texnr();
-	GLfloat vertex_bottom();
-	GLfloat vertex_top();
-	GLfloat vertex_left();
-	GLfloat vertex_right();
-	void setViewport(int w, int h);
-	void zoom(float delta, QPoint mousepos);
-	void reset();
-	void move(QPoint diff);
-	bool setSize(QSize size);
-	void rotate();
-	void zoomToOriginal();
-	
-protected:
-	bool _load();
-	void calcVertex();
+namespace KIPIviewer 
+{
 
-	int display_x, display_y;
-	GLuint _texnr;
-	QSize initial_size;
-	QString filename;
-	QImage qimage, glimage;
-	float rdx,rdy,z,ux,uy,rtx,rty;
-	float vtop, vbottom, vleft, vright;
-	int rotate_list[4], rotate_idx;
+class Texture
+{
+
+public:
+
+    Texture();
+    ~Texture();
+
+    int height();
+    int width();
+    bool load(QString fn, QSize size, GLuint tn);
+    bool load(QImage im, QSize size, GLuint tn);
+    GLvoid * data();
+    GLuint texnr();
+    GLfloat vertex_bottom();
+    GLfloat vertex_top();
+    GLfloat vertex_left();
+    GLfloat vertex_right();
+    void setViewport(int w, int h);
+    void zoom(float delta, QPoint mousepos);
+    void reset();
+    void move(QPoint diff);
+    bool setSize(QSize size);
+    void rotate();
+    void zoomToOriginal();
+
+protected:
+
+    bool _load();
+    void calcVertex();
+
+    int display_x, display_y;
+    GLuint _texnr;
+    QSize initial_size;
+    QString filename;
+    QImage qimage, glimage;
+    float rdx,rdy,z,ux,uy,rtx,rty;
+    float vtop, vbottom, vleft, vright;
+    int rotate_list[4], rotate_idx;
 };
+
 }; //namespace KIPIviewer
 
-#endif
+#endif // TEXTURE_H
