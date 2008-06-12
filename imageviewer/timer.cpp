0789c775eed288da26e5b4bd1a351fa111107129
Gilles Caulier <caulier.gilles@gmail.com>
2008-06-12 10:06:31 +0000

    fix header remove tab polish
    svn path=/trunk/extragear/graphics/kipi-plugins/; revision=819838

--- imageviewer/timer.cpp
+++ imageviewer/timer.cpp
@@ -1,23 +1,32 @@
-/***************************************************************************
- *   Copyright (C) 2007-2008 by Markus Leuthold   *
- *   <kusi (+at) forum.titlis.org>   *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA.        *
- ***************************************************************************/
+/* ============================================================
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2007-02-11
+ * Description : a kipi plugin to show image using 
+ *               an OpenGL interface.
+ *
+ * Copyright (C) 2007-2008 by Markus Leuthold <kusi at forum dot titlis dot org>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+// KDE includes.
+
 #include <kdebug.h>
+
+// Local includes.
+
 #include "timer.h"
 
 using namespace KIPIviewer;
@@ -28,18 +37,16 @@ Timer::Timer()
 
 void Timer::start() 
 {
-	timer.start();
-	meantime=0;
+    timer.start();
+    meantime=0;
 }
 
 void Timer::at(QString s)
 {
-	meantime=timer.elapsed()-meantime;
-	kDebug() << "stopwatch:"<< s << ": " << meantime << " ms    overall: " << timer.elapsed() << " ms";
+    meantime=timer.elapsed()-meantime;
+    kDebug() << "stopwatch:"<< s << ": " << meantime << " ms    overall: " << timer.elapsed() << " ms";
 }
 
 Timer::~Timer()
 {
 }
-
-
--- imageviewer/timer.h
+++ imageviewer/timer.h
@@ -1,24 +1,31 @@
-/***************************************************************************
- *   Copyright (C) 2007 by Markus Leuthold   *
- *   <kusi at forum.titlis.org>   *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA.        *
- ***************************************************************************/
+/* ============================================================
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2007-02-11
+ * Description : a kipi plugin to show image using 
+ *               an OpenGL interface.
+ *
+ * Copyright (C) 2007-2008 by Markus Leuthold <kusi at forum dot titlis dot org>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
 #ifndef TIMER_H
 #define TIMER_H
+
+// Qt includes.
+
 #include <qstring.h>
 #include <qdatetime.h>
 
@@ -27,17 +34,25 @@
  * @author Markus Leuthold <kusi (+at) forum.titlis.org>
  * @version 0.2
  */
+namespace KIPIviewer 
+{
 
-namespace KIPIviewer {
-class Timer{
+class Timer
+{
+
 public:
+
     Timer();
     ~Timer();
-	void start();
-	void at(QString s);
+    void start();
+    void at(QString s);
+
 protected:
-	QTime timer;
-	int meantime;
+
+    QTime timer;
+    int meantime;
 };
+
 }; //namespace KIPIviewer
-#endif
+
+#endif // TIMER_H
