aa37f6f23a55117d0666c82984ff61f4367ac09b
Andrea Diamantini <adjam7@gmail.com>
2008-08-09 14:56:42 +0000

    Adjusted *cpp/*.h headers to follow Debian (C) issues. But I really don't know.. why should we follow Debian's??
    svn path=/trunk/extragear/graphics/kipi-plugins/; revision=844379

--- galleryexport/galleryconfig.cpp
+++ galleryexport/galleryconfig.cpp
@@ -1,27 +1,29 @@
 /* ============================================================
- * File  : galleryconfig.cpp
- * Author: Colin Guthrie <kde@colin.guthr.ie>
- * Date  : 2006-09-04
- * Copyright 2006 by Colin Guthrie <kde@colin.guthr.ie>
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2003-10-24
+ * Description : 
+ *
+ * Copyright (C) 2003-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
+ * Copyright (C) 2006 by Colin Guthrie <kde@colin.guthr.ie>
+ * Copyright (C) 2006-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2008 by Andrea Diamantini <adjam7 at gmail dot com>
+ *
  *
- *
- * Modified by : Andrea Diamantini <adjam7@gmail.com>
- * Date        : 2008-07-11
- * Copyright 2008 by Andrea Diamantini <adjam7@gmail.com>
- *
- *
- * This program is free software; you can redistribute it
- * and/or modify it under the terms of the GNU General
- * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option)
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * ============================================================ */
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
 
 
 // Include files for Qt
