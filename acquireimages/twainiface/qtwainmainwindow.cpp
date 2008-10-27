d3fc7999564eef3d0518dc3825207109c037f9df
Gilles Caulier <caulier.gilles@gmail.com>
2008-10-27 14:17:00 +0000

    polish
    svn path=/trunk/extragear/graphics/kipi-plugins/; revision=876567

--- acquireimages/twainiface/qtwainmainwindow.cpp
+++ acquireimages/twainiface/qtwainmainwindow.cpp
@@ -1,22 +1,25 @@
-/*
-QTwain interface class set
-Copyright (C) 2002-2003         Stephan Stapel
-                                stephan.stapel@web.de
-
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+/* ============================================================
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2008-27-10
+ * Description : Twain interface
+ *
+ * Copyright (C) 2002-2003 Stephan Stapel <stephan dot stapel at web dot de>
+ * Copyright (C) 2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
 
 #include <QPainter>
 
@@ -27,114 +30,94 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 
 QTwainMainWindow::QTwainMainWindow(QWidget* parent, Qt::WindowFlags f )
-	: QMainWindow(parent, f)
+                : QMainWindow(parent, f)
 {
-	m_pTwain         = new QTwain(0);
-    m_pWidget        = new QWidget(this);	// do this as your first call!
-	m_pVBox          = new QGridLayout();
-	m_pWidget->setLayout(m_pVBox);
-	m_pAcquireButton = new QPushButton("Acquire image",m_pWidget);
-	m_pSelectButton  = new QPushButton("Select source",m_pWidget);
-	m_pLabel = new QLabel();
-	m_pVBox->addWidget(m_pAcquireButton,0,0,0);
+    m_pTwain         = new QTwain(0);
+    m_pWidget        = new QWidget(this);    // do this as your first call!
+    m_pVBox          = new QGridLayout();
+    m_pWidget->setLayout(m_pVBox);
+    m_pAcquireButton = new QPushButton("Acquire image",m_pWidget);
+    m_pSelectButton  = new QPushButton("Select source",m_pWidget);
+    m_pLabel         = new QLabel();
+
+    m_pVBox->addWidget(m_pAcquireButton,0,0,0);
     m_pVBox->addWidget(m_pSelectButton,0,1,0);
-	m_pVBox->addWidget(m_pLabel,1,0,2,2,0);
-	 m_pWidget->setMinimumSize(500,500);
-	m_pPixmap        = 0;
-	
- 
-    
+    m_pVBox->addWidget(m_pLabel,1,0,2,2,0);
+    m_pWidget->setMinimumSize(500,500);
+    m_pPixmap        = 0;
 
-	QObject::connect(m_pSelectButton, SIGNAL(clicked()),
-	                 this, SLOT(onSelectButton()));
-	QObject::connect(m_pAcquireButton, SIGNAL(clicked()),
-	                 this, SLOT(onAcquireButton()));
-	QObject::connect(m_pTwain, SIGNAL(dibAcquired(CDIB*)),
-		             this, SLOT(onDibAcquired(CDIB*)));
+    connect(m_pSelectButton, SIGNAL(clicked()),
+            this, SLOT(onSelectButton()));
 
-	
-	setMinimumSize(800, 600);
-	
-	setGeometry ( 300,300 ,800,600 ) ;
-} // !QTwainMainWindow()
+    connect(m_pAcquireButton, SIGNAL(clicked()),
+            this, SLOT(onAcquireButton()));
 
+    connect(m_pTwain, SIGNAL(dibAcquired(CDIB*)),
+            this, SLOT(onDibAcquired(CDIB*)));
 
+    setMinimumSize(800, 600);
+    setGeometry ( 300,300 ,800,600 ) ;
+}
 
 QTwainMainWindow::~QTwainMainWindow()
 {
-	delete m_pTwain;
-} // !~QTwainMainWindow()
-
-
+    delete m_pTwain;
+}
 
 void QTwainMainWindow::showEvent(QShowEvent* thisEvent)
 {
-	// set the parent here to be sure to have a really
-	// valid window as the twain parent!
-	m_pTwain->setParent(this);
-} // !showEvent()
-
-
+    // set the parent here to be sure to have a really
+    // valid window as the twain parent!
+    m_pTwain->setParent(this);
+}
 
 bool QTwainMainWindow::winEvent(MSG* pMsg, long* result)
-{ qWarning("QTwainMainWindow::winEvent");
-	//return (m_pTwain->processMessage(*pMsg) == true);
-	m_pTwain->processMessage(*pMsg); 
-	return false;
-} // !winEvent()
-
-
+{
+    //return (m_pTwain->processMessage(*pMsg) == true);
+    m_pTwain->processMessage(*pMsg); 
+    return false;
+}
 
 void QTwainMainWindow::resizeEvent(QResizeEvent* thisEvent)
 {
-	//this->setGeometry(0, 0, 170, 70);
-} // !resizeEvent()
-
-
+    //this->setGeometry(0, 0, 170, 70);
+}
 
 void QTwainMainWindow::onSelectButton()
 {
-	m_pTwain->selectSource();
-} // !onSelectButton()
-
-
+    m_pTwain->selectSource();
+}
 
 void QTwainMainWindow::onAcquireButton()
 {
-	if (!m_pTwain->acquire())
-	{
-		qWarning("acquire() call not successful!");
-	}
-} // !onAcquireButton()
-
-
+    if (!m_pTwain->acquire())
+    {
+        qWarning("acquire() call not successful!");
+    }
+}
 
 void QTwainMainWindow::onDibAcquired(CDIB* pDib)
 {
-	if (m_pPixmap)
-		delete m_pPixmap;
+    if (m_pPixmap)
+        delete m_pPixmap;
 
-	m_pPixmap = QTwainInterface::convertToPixmap(pDib);	
-	
-   m_pLabel->setPixmap(*m_pPixmap);
-	delete pDib;
-	
-	//repaint();
-} // !onDibAcquired()
+    m_pPixmap = QTwainInterface::convertToPixmap(pDib);
 
-
+    m_pLabel->setPixmap(*m_pPixmap);
+    delete pDib;
+    //repaint();
+}
 
 void QTwainMainWindow::paintEvent(QPaintEvent* thisEvent)
 {
-	if (m_pPixmap)
-	{
-	  qWarning("paintEvent");
-		//QPainter p(m_pWidget);
-		//m_pVBox->addWidget(p,1,0,0);
-		/*p.drawPixmap(0, 71, 
-			         *m_pPixmap,
-					 0, 0,
-					 width(), height() - 71);
-					 */
-	}
-} // !paintEvent()
+    if (m_pPixmap)
+    {
+        //QPainter p(m_pWidget);
+        //m_pVBox->addWidget(p,1,0,0);
+        /*p.drawPixmap(0, 71, 
+                     *m_pPixmap,
+                     0, 0,
+                     width(), height() - 71);
+                     */
+    }
+}
--- acquireimages/twainiface/qtwainmainwindow.h
+++ acquireimages/twainiface/qtwainmainwindow.h
@@ -1,26 +1,28 @@
-/*
-QTwain interface class set
-Copyright (C) 2002-2003         Stephan Stapel
-                                stephan.stapel@web.de
-
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+/* ============================================================
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2008-27-10
+ * Description : Twain interface
+ *
+ * Copyright (C) 2002-2003 Stephan Stapel <stephan dot stapel at web dot de>
+ * Copyright (C) 2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
 
 #ifndef _QTWAINMAINWINDOW_H_
 #define _QTWAINMAINWINDOW_H_
-
 
 #include <QMainWindow>
 #include <QPushButton>
@@ -30,50 +32,50 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <QLabel>
 #include "qtwaininterface.h"
 
-
 class QTwainMainWindow : public QMainWindow
 {
-	Q_OBJECT
+    Q_OBJECT
+
 public:
-	/** 
-	 * Standard constructor
-	 */
-	QTwainMainWindow(QWidget* parent = 0,  Qt::WindowFlags f =  Qt::Window);
 
-	/**
-	 * Standard destructor
-	 */
-	virtual ~QTwainMainWindow();
+    /**
+     * Standard constructor
+     */
+    QTwainMainWindow(QWidget* parent = 0,  Qt::WindowFlags f =  Qt::Window);
 
-	/**
-	 * @name overwritten to hook-in twain interface
-	 */
-	/*@{*/
-	void showEvent(QShowEvent* thisEvent);
-	
-	/*@}*/
+    /**
+     * Standard destructor
+     */
+    virtual ~QTwainMainWindow();
 
-protected:	
-	void paintEvent(QPaintEvent* thisEvent);
-	void resizeEvent(QResizeEvent* thisEvent);
-	virtual bool winEvent(MSG* pMsg,long * result);
-
-private slots:
-	void onSelectButton();
-	void onAcquireButton();
-
-	void onDibAcquired(CDIB* pDib);
-
+    /**
+     * @name overwritten to hook-in twain interface
+     */
+    /*@{*/
+    void showEvent(QShowEvent* thisEvent);
+    /*@}*/
 
 protected:
-	QTwainInterface* m_pTwain;
-	QPushButton* m_pSelectButton;
-	QPushButton* m_pAcquireButton;
-	QPixmap* m_pPixmap;
-	QGridLayout* m_pVBox;
-	QWidget *m_pWidget;
-	QLabel *m_pLabel;
-};
 
+    void paintEvent(QPaintEvent* thisEvent);
+    void resizeEvent(QResizeEvent* thisEvent);
+    virtual bool winEvent(MSG* pMsg,long * result);
+
+private slots:
+
+    void onSelectButton();
+    void onAcquireButton();
+    void onDibAcquired(CDIB* pDib);
+
+protected:
+
+    QTwainInterface* m_pTwain;
+    QPushButton*     m_pSelectButton;
+    QPushButton*     m_pAcquireButton;
+    QPixmap*         m_pPixmap;
+    QGridLayout*     m_pVBox;
+    QWidget*         m_pWidget;
+    QLabel*          m_pLabel;
+};
 
 #endif // !_QTWAINMAINWINDOW_H_
