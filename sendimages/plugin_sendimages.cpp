fc6898dfe5b2700bc5a1476202db53e07cd06f98
Gilles Caulier <caulier.gilles@gmail.com>
2008-11-19 10:19:07 +0000

    there is no icon to install
    svn path=/trunk/extragear/graphics/kipi-plugins/; revision=886457

--- sendimages/plugin_sendimages.cpp
+++ sendimages/plugin_sendimages.cpp
@@ -6,7 +6,7 @@
  * Date        : 2003-10-01
  * Description : a kipi plugin to e-mailing images
  *
- * Copyright (C) 2003-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2003-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -19,7 +19,7 @@
  * GNU General Public License for more details.
  *
  * ============================================================ */
- 
+
 #include "plugin_sendimages.h"
 #include "plugin_sendimages.moc"
 
@@ -58,7 +58,7 @@ public:
     }
 
    KAction                          *action_sendimages;
-   
+
    KIPISendimagesPlugin::SendImages *sendImagesOperation;
 };
 
--- sendimages/plugin_sendimages.h
+++ sendimages/plugin_sendimages.h
@@ -6,7 +6,7 @@
  * Date        : 2003-10-01
  * Description : a kipi plugin to e-mailing images
  *
- * Copyright (C) 2003-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2003-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -47,14 +47,14 @@ public:
 
    Plugin_SendImages(QObject *parent, const QVariantList &args);
    ~Plugin_SendImages();
-   
+
    virtual KIPI::Category category( KAction* action ) const;
    virtual void setup( QWidget* );
-  
+
 public slots:
 
    void slotActivate();
-  
+
 private:
 
     Plugin_SendImagesPriv *d;
