9c598c514310e46fc74712a26818026e6df2435f
Gilles Caulier <caulier.gilles@gmail.com>
2008-11-19 07:46:14 +0000

    fix header missing moc file
    svn path=/trunk/extragear/graphics/kipi-plugins/; revision=886400

--- calendar/calwidget.cpp
+++ calendar/calwidget.cpp
@@ -1,19 +1,19 @@
 /* ============================================================
- * File  : calwidget.cpp
- * Authors: Renchi Raju <renchi@pooh.tam.uiuc.edu>
- *          Orgad Shaneh <orgads@gmail.com>
- * Date  : 2008-11-13
- * Description: calendar widget used for showing a preview
- *              of the active template
  *
- * Copyright 2003 by Renchi Raju
- * Copyright 2008 by Orgad Shaneh
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2003-11-03
+ * Description :  calendar widget used for showing a preview
+ *                of the active template.
+ *
+ * Copyright (C) 2003-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
+ * Copyright (C) 2007-2008 by Orgad Shaneh <orgads at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option)
- * any later version.
+ * either version 2, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -24,15 +24,16 @@
 
 // Local includes.
 
-#include "calwidget.h"
 #include "calpainter.h"
 #include "calsettings.h"
+#include "calwidget.h"
+#include "calwidget.moc"
 
 namespace KIPICalendarPlugin
 {
 
 CalWidget::CalWidget(QWidget *parent)
-    : QWidget(parent,0)
+         : QWidget(parent,0)
 {
     setAttribute(Qt::WA_NoBackground);
     setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
--- calendar/calwidget.h
+++ calendar/calwidget.h
@@ -1,25 +1,25 @@
 /* ============================================================
- * File  : calwidget.h
- * Authors: Renchi Raju <renchi@pooh.tam.uiuc.edu>
- *          Orgad Shaneh <orgads@gmail.com>
- * Date  : 2008-11-13
- * Description: calendar widget used for showing a preview
- *              of the active template
  *
- * Copyright 2003 by Renchi Raju
- * Copyright 2008 by Orgad Shaneh
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2003-11-03
+ * Description :  calendar widget used for showing a preview
+ *                of the active template.
+ *
+ * Copyright (C) 2003-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
+ * Copyright (C) 2007-2008 by Orgad Shaneh <orgads at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option)
- * any later version.
- * 
+ * either version 2, or (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * ============================================================ */
 
 #ifndef __CALWIDGET_H__
@@ -51,7 +51,6 @@ public slots:
 protected:
 
     void paintEvent(QPaintEvent *e);
-
 };
 
 }  // NameSpace KIPICalendarPlugin
