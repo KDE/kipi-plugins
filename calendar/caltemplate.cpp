b6e1df960140bf0ddf8f2d9a04378e379ac0e467
Gilles Caulier <caulier.gilles@gmail.com>
2008-11-19 07:48:08 +0000

    fix header
    svn path=/trunk/extragear/graphics/kipi-plugins/; revision=886401

--- calendar/caltemplate.cpp
+++ calendar/caltemplate.cpp
@@ -1,18 +1,18 @@
 /* ============================================================
- * File  : caltemplate.cpp
- * Authors: Renchi Raju <renchi@pooh.tam.uiuc.edu>
- *          Orgad Shaneh <orgads@gmail.com>
- * Date  : 2008-11-13
- * Description: template selection for calendar
  *
- * Copyright 2003 by Renchi Raju
- * Copyright 2008 by Orgad Shaneh
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2003-11-03
+ * Description : template selection for calendar.
+ *
+ * Copyright (C) 2003-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
+ * Copyright (C) 2007-2008 by Orgad Shaneh <orgads at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option)
- * any later version.
+ * either version 2, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -45,16 +45,20 @@ CalTemplate::CalTemplate(QWidget* parent)
 
     ui.calendarWidget->recreate();
 
-    connect(ui.paperSizeCombo, SIGNAL(currentIndexChanged(const QString &)),
-            settings, SLOT(setPaperSize(const QString &)));
+    connect(ui.paperSizeCombo, SIGNAL(currentIndexChanged(const QString&)),
+            settings, SLOT(setPaperSize(const QString&)));
+
     connect(ui.imagePosButtonGroup, SIGNAL(changed(int)),
             settings, SLOT(setImagePos(int)));
+
     connect(ui.drawLinesCheckBox, SIGNAL(toggled(bool)),
             settings, SLOT(setDrawLines(bool)));
+
     connect(ui.ratioSlider, SIGNAL(valueChanged(int)),
             settings, SLOT(setRatio(int)));
-    connect(ui.fontCombo, SIGNAL(currentIndexChanged(const QString &)),
-            settings, SLOT(setFont(const QString &)));
+
+    connect(ui.fontCombo, SIGNAL(currentIndexChanged(const QString&)),
+            settings, SLOT(setFont(const QString&)));
 
     connect(settings, SIGNAL(settingsChanged()),
             ui.calendarWidget, SLOT(recreate()));
--- calendar/caltemplate.h
+++ calendar/caltemplate.h
@@ -1,18 +1,18 @@
 /* ============================================================
- * File  : caltemplate.h
- * Authors: Renchi Raju <renchi@pooh.tam.uiuc.edu>
- *          Orgad Shaneh <orgads@gmail.com>
- * Date  : 2008-11-13
- * Description: template selection for calendar
  *
- * Copyright 2003 by Renchi Raju
- * Copyright 2008 by Orgad Shaneh
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2003-11-03
+ * Description : template selection for calendar.
+ *
+ * Copyright (C) 2003-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
+ * Copyright (C) 2007-2008 by Orgad Shaneh <orgads at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option)
- * any later version.
+ * either version 2, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -28,7 +28,7 @@
 
 #include <QWidget>
 
-// UI include.
+// Local include.
 
 #include "ui_caltemplate.h"
 
@@ -45,7 +45,6 @@ public:
 private:
 
     Ui::CalTemplate ui;
-
 };
 
 }  // NameSpace KIPICalendarPlugin
