e9aa7e58048e9f568eb9c83bb27d31827df54ca3
Andi Clemens <andi.clemens@gmx.net>
2008-12-17 20:50:54 +0000

    GalleryExport kipi-plugin: fix registration in ActionCollection
    svn path=/trunk/extragear/graphics/kipi-plugins/; revision=898257

--- galleryexport/plugin_galleryexport.cpp
+++ galleryexport/plugin_galleryexport.cpp
@@ -1,33 +1,34 @@
 /* ============================================================
- *
- * This file is a part of kipi-plugins project
- * http://www.kipi-plugins.org
- *
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
  * Date        : 2003-10-01
  * Description : a plugin to export to a remote Gallery server.
  *
- * Copyright (C) 2003-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
- * Copyright (C) 2006 by Colin Guthrie <kde@colin.guthr.ie>
- * Copyright (C) 2006-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2003-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
+ * Copyright (C) 2006 by Colin Guthrie <kde@colin.guthr.ie>
+ * Copyright (C) 2006-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2008 by Andrea Diamantini <adjam7 at gmail dot com>
  *
- * This program is free software; you can redistribute it
- * and/or modify it under the terms of the GNU General
- * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Â See the
- * GNU General Public License for more details.
- *
- * ============================================================ */
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Â See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
 
 #include "plugin_galleryexport.h"
 
 // KDE includes.
 
 #include <kaction.h>
+#include <kactioncollection.h>
 #include <kapplication.h>
 #include <kdebug.h>
 #include <kgenericfactory.h>
@@ -72,11 +73,14 @@ void Plugin_GalleryExport::setup(QWidget* widget)
         return;
     }
 
-    m_action = new KAction(i18n("remote-gallery-sync"), this);
+    m_action = actionCollection()->addAction("galleryexport");
     m_action->setText(i18n("Export to Gallery..."));
     m_action->setIcon(KIcon("applications-internet"));
-    connect(m_action, SIGNAL(triggered(bool)), this, SLOT(slotSync()));
     m_action->setEnabled(true);
+
+    connect(m_action, SIGNAL(triggered(bool)),
+            this, SLOT(slotSync()));
+
     addAction(m_action);
 }
 
