9c9ebd0f468a319224ebc4c24573060853ddc388
Luka Renko <lure@kubuntu.org>
2009-01-01 17:20:00 +0000

    Almost forgot it is new year - fix copyright date. ;-)
    svn path=/trunk/extragear/graphics/kipi-plugins/; revision=904228

--- fbexport/fbitem.h
+++ fbexport/fbitem.h
@@ -6,7 +6,7 @@
  * Date        : 2008-12-26
  * Description : a kipi plugin to export images to Facebook web service
  *
- * Copyright (C) 2008 by Luka Renko <lure at kubuntu dot org>
+ * Copyright (C) 2008-2009 by Luka Renko <lure at kubuntu dot org>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
