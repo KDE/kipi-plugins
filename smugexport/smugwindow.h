4549de0e6b9dabb3a049514ba5790d2cd4aab737
Andi Clemens <andi.clemens@gmx.net>
2009-01-01 17:41:30 +0000

    a new year
    svn path=/trunk/extragear/graphics/kipi-plugins/; revision=904234

--- smugexport/smugwindow.h
+++ smugexport/smugwindow.h
@@ -7,8 +7,8 @@
  * Description : a kipi plugin to export images to SmugMug web service
  *
  * Copyright (C) 2005-2009 by Vardhman Jain <vardhman at gmail dot com>
- * Copyright (C) 2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
- * Copyright (C) 2008 by Luka Renko <lure at kubuntu dot org>
+ * Copyright (C) 2008-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2008-2009 by Luka Renko <lure at kubuntu dot org>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
