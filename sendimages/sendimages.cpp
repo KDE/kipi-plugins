9e7dfe5b42566b5ed534c6c99372a116df08dba9
Andi Clemens <andi.clemens@gmx.net>
2009-01-01 17:31:10 +0000

    a new year
    svn path=/trunk/extragear/graphics/kipi-plugins/; revision=904232

--- sendimages/sendimages.cpp
+++ sendimages/sendimages.cpp
@@ -6,7 +6,7 @@
  * Date        : 2004-02-25
  * Description : a kipi plugin to e-mailing images
  *
- * Copyright (C) 2004-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2004-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2006 by Tom Albers <tomalbers at kde dot nl>
  * Copyright (C) 2006 by Michael Hoechstetter <michael dot hoechstetter at gmx dot de>
  *
