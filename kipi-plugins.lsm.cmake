176d22692dc167a4356a16c0ac201eca216a9e72
Gilles Caulier <caulier.gilles@gmail.com>
2009-01-21 09:09:26 +0000

    update
    svn path=/trunk/extragear/graphics/kipi-plugins/; revision=914491

--- kipi-plugins.lsm.cmake
+++ kipi-plugins.lsm.cmake
@@ -1,18 +1,18 @@
 Begin4
-Title: kipi-plugins
-Version: ${KIPIPLUGINS_VERSION_STRING}
-Entered-date: 2009-01-21
-Description: Plugins for KIPI, the KDE Image Plugin Interface
-Keywords: kde image plugins, digiKam, photo management
-Author: caulier dot gilles at gmail dot com (Gilles Caulier)
- marcel dot wiesweg at gmx dot de (Marcel Wiesweg)
- renchi at pooh dot tam dot uiuc dot edu (Renchi Raju)
- blackie at kde dot org (Jesper K. Pedersen)
- aurelien dot gateau at free dot fr (Aurelien Gateau)
+Title:          kipi-plugins
+Version:        ${KIPIPLUGINS_VERSION_STRING}
+Entered-date:   2009-01-21
+Description:    Plugins for KIPI, the KDE Image Plugin Interface
+Keywords:       kde image plugins, digiKam, photo management, KDE, Graphics, Digital Photo Management, Exif, Iptc, Makenotes, Xmp, Geolocation, Batch process, Raw workflow
+Author:         caulier dot gilles at gmail dot com (Gilles Caulier)
+                marcel dot wiesweg at gmx dot de (Marcel Wiesweg)
+                aurelien dot gateau at free dot fr (Aurelien Gateau)
+                blackie at kde dot org (Jesper K. Pedersen)
+                renchi at pooh dot tam dot uiuc dot edu (Renchi Raju)
 Maintained-by:
-Primary-site: http://www.kipi-plugins.org
+Primary-site:   http://www.kipi-plugins.org
 Alternate-site: http://www.digikam.org
 Original-site:
-Platforms: Linux, MacOS-X, Windows
+Platforms:      Linux and other Unices, MacOs-X, Win32. Needs KDE 4.x
 Copying-policy: GPL
 End
