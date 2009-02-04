af57db435ed96e05e3c7a2dc9697f22bab7f4300
Gilles Caulier <caulier.gilles@gmail.com>
2009-02-04 12:42:04 +0000

    use const d
    svn path=/trunk/extragear/graphics/kipi-plugins/; revision=921052

--- common/libkipiplugins/kpwriteimage.cpp
+++ common/libkipiplugins/kpwriteimage.cpp
@@ -7,7 +7,7 @@
  * Description : Kipi-Plugins shared library.
  *               Interface to write image data to common picture format.
  *
- * Copyright (C) 2007-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2007-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -87,8 +87,8 @@ public:
 };
 
 KPWriteImage::KPWriteImage()
+            : d(new KPWriteImagePriv)
 {
-    d = new KPWriteImagePriv;
 }
 
 KPWriteImage::~KPWriteImage()
