993bbd74f25d7135e97657014cd8f1560efad7fc
Gilles Caulier <caulier.gilles@gmail.com>
2009-02-24 13:14:11 +0000

    optimize space on header
    svn path=/trunk/extragear/graphics/kipi-plugins/; revision=930855

--- common/libkipiplugins/imageslist.cpp
+++ common/libkipiplugins/imageslist.cpp
@@ -6,8 +6,8 @@
  * Date        : 2008-05-21
  * Description : widget to display an imagelist
  *
- * Copyright (C) 2006-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
- * Copyright (C) 2008 by Andi Clemens <andi dot clemens at gmx dot net>
+ * Copyright (C) 2006-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2008-2009 by Andi Clemens <andi dot clemens at gmx dot net>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -178,7 +178,13 @@ ImagesListView::ImagesListView(ImagesList *parent)
     hideColumn(User4);
     hideColumn(User5);
     hideColumn(User6);
-    header()->setResizeMode(QHeaderView::ResizeToContents);
+
+    header()->setResizeMode(User1, QHeaderView::ResizeToContents);
+    header()->setResizeMode(User2, QHeaderView::Stretch);
+    header()->setResizeMode(User3, QHeaderView::Stretch);
+    header()->setResizeMode(User4, QHeaderView::Stretch);
+    header()->setResizeMode(User5, QHeaderView::Stretch);
+    header()->setResizeMode(User6, QHeaderView::Stretch);
 }
 
 ImagesListView::~ImagesListView()
