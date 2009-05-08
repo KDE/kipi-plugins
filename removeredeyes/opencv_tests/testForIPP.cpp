67967b9966cb3215e4c6cdadf61439fe8c02e7a8
Andi Clemens <andi.clemens@gmx.net>
2009-05-08 09:32:14 +0000

    add missing license header
    svn path=/trunk/extragear/graphics/kipi-plugins/; revision=965157

--- removeredeyes/opencv_tests/testForIPP.cpp
+++ removeredeyes/opencv_tests/testForIPP.cpp
@@ -1,3 +1,28 @@
+/* ============================================================
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2009-05-08
+ * Description : this is a test program to check if
+ *               OpenCV is using the IPP infrastructure
+ *               of Intel CPUs
+ *
+ * Copyright (C) 2009 by Andi Clemens <andi dot clemens at gmx dot net>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
 #include <cv.h>
 #include <cstdio>
 
