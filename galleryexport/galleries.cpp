552474135d5c846c3732e42a4830c592eb74f9a3
Andi Clemens <andi.clemens@gmx.net>
2009-07-11 09:29:53 +0000

    A warning should only be used when something is going wrong, not for normal debugging output. Otherwise the applications using this plugin will never start when using QT_FATAL_WARNINGS as an environment variable.
    svn path=/trunk/extragear/graphics/kipi-plugins/; revision=994725

--- galleryexport/galleries.cpp
+++ galleryexport/galleries.cpp
@@ -136,7 +136,7 @@ void Gallery::load()
     KConfig config("kipirc");
     KConfigGroup group = config.group("Gallery Settings");
 
-    kWarning( 51000 ) << "Reading data from kipirc file.." << endl;
+    kDebug( 51000 ) << "Reading data from kipirc file.." << endl;
 
     mName = group.readEntry("Name", QString() );
     mUrl = group.readEntry("URL", QString() );
@@ -153,7 +153,7 @@ void Gallery::save()
     KConfig config("kipirc");
     KConfigGroup group = config.group("Gallery Settings");
 
-    kWarning( 51000 ) << "Saving data to kipirc file.." << endl;
+    kDebug( 51000 ) << "Saving data to kipirc file.." << endl;
 
     group.writeEntry(QString("Name"), name() );
     group.writeEntry(QString("URL"), url() );
