b09e9b503d7506a309bdb7f66f60f6ccddf78d84
Michael Georg Hansen <mike@mghansen.de>
2009-10-02 19:15:40 +0000

    Update e-mail address.
    SVN_SILENT
    
    svn path=/trunk/extragear/graphics/kipi-plugins/; revision=1030622

--- gpssync/gpslistviewcontextmenu.cpp
+++ gpssync/gpslistviewcontextmenu.cpp
@@ -7,7 +7,7 @@
  * Description : Context menu for GPS list view which can be used
  *               in the track list editor and the sync dialog
  *
- * Copyright (C) 2009 by Michael G. Hansen <mhansen at mghansen dot de>
+ * Copyright (C) 2009 by Michael G. Hansen <mike at mghansen dot de>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
