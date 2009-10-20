4a1e5a827e5ac83130bf2a739b22e643cd6779d5
Andi Clemens <andi.clemens@gmx.net>
2009-10-20 11:54:15 +0000

    fix pedantic
    svn path=/trunk/extragear/graphics/kipi-plugins/; revision=1037980

--- printimages/templateicon.h
+++ printimages/templateicon.h
@@ -69,7 +69,7 @@ class TemplateIcon
     QSize &getSize( void );
 
     /**
-        Draw a filled rectangle with color <color> at position <x>/<y> (relative 
+        Draw a filled rectangle with color <color> at position <x>/<y> (relative
         to template-origin) and width <w> and height <h>.
     */
     void fillRect( int x, int y, int w, int h, QColor color );
@@ -86,6 +86,7 @@ class TemplateIcon
     QPainter *painter;
     QIcon    *icon;
 };
-}; // namespace KIPIPrintImagesPlugin
+
+} // namespace KIPIPrintImagesPlugin
 
 #endif /* PREVIEW_ICON_H */
