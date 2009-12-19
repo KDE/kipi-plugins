2cd2a2a0afd300cda977dc3303febd424c5dee26
Gilles Caulier <caulier.gilles@gmail.com>
2009-12-19 15:02:11 +0000

    factorize
    svn path=/trunk/extragear/graphics/kipi-plugins/; revision=1063907

---
+++ common/libkipiplugins/wizardpage.cpp
@@ -0,0 +1,116 @@
+/* ============================================================
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2009-11-13
+ * Description : a plugin to blend bracketed images.
+ *
+ * Copyright (C) 2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include "wizardpage.h"
+
+// Qt includes
+
+#include <QHBoxLayout>
+#include <QLabel>
+
+// KDE includes
+
+#include <kvbox.h>
+#include <kiconloader.h>
+#include <kseparator.h>
+#include <kstandarddirs.h>
+#include <kassistantdialog.h>
+#include <kpagewidgetmodel.h>
+
+namespace KIPIPlugins
+{
+
+class WizardPagePriv
+{
+public:
+
+    WizardPagePriv()
+    {
+        hlay          = 0;
+        page          = 0;
+        logo          = 0;
+        leftBottomPix = 0;
+    }
+
+    QLabel*          logo;
+    QLabel*          leftBottomPix;
+
+    QHBoxLayout*     hlay;
+
+    KPageWidgetItem* page;
+};
+
+WizardPage::WizardPage(KAssistantDialog* dlg, const QString& title)
+          : QScrollArea(dlg), d(new WizardPagePriv)
+{
+    QWidget *panel = new QWidget(viewport());
+    panel->setAutoFillBackground(false);
+    setWidget(panel);
+    setWidgetResizable(true);
+    viewport()->setAutoFillBackground(false);
+
+    d->hlay     = new QHBoxLayout(panel);
+    KVBox *vbox = new KVBox(panel);
+    d->logo     = new QLabel(vbox);
+    d->logo->setAlignment(Qt::AlignTop);
+    d->logo->setPixmap(QPixmap(KStandardDirs::locate("data", "kipi/data/kipi-plugins_logo.png"))
+                               .scaled(128, 128, Qt::KeepAspectRatio, Qt::SmoothTransformation));
+
+    QLabel *space    = new QLabel(vbox);
+    d->leftBottomPix = new QLabel(vbox);
+    d->leftBottomPix->setAlignment(Qt::AlignBottom);
+    vbox->setStretchFactor(space, 10);
+    vbox->setMargin(KDialog::spacingHint());
+    vbox->setSpacing(KDialog::spacingHint());
+
+    KSeparator *line = new KSeparator(Qt::Vertical, panel);
+
+    d->hlay->addWidget(vbox);
+    d->hlay->addWidget(line);
+    d->hlay->setMargin(0);
+    d->hlay->setSpacing(KDialog::spacingHint());
+
+    d->page = dlg->addPage(this, title);
+}
+
+WizardPage::~WizardPage()
+{
+    delete d;
+}
+
+KPageWidgetItem* WizardPage::page() const
+{
+    return d->page;
+}
+
+void WizardPage::setPageWidget(QWidget* w)
+{
+    d->hlay->addWidget(w);
+    d->hlay->setStretchFactor(w, 10);
+}
+
+void WizardPage::setLeftBottomPix(const QPixmap& pix)
+{
+    d->leftBottomPix->setPixmap(pix);
+}
+
+}   // namespace KIPIPlugins
---
+++ common/libkipiplugins/wizardpage.h
@@ -0,0 +1,64 @@
+/* ============================================================
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2009-11-13
+ * Description : a plugin to blend bracketed images.
+ *
+ * Copyright (C) 2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef WIZARD_PAGE_H
+#define WIZARD_PAGE_H
+
+// Qt includes
+
+#include <QScrollArea>
+#include <QString>
+#include <QPixmap>
+
+// Local includes
+
+#include "kipiplugins_export.h"
+
+class KAssistantDialog;
+class KPageWidgetItem;
+
+namespace KIPIPlugins
+{
+
+class WizardPagePriv;
+
+class KIPIPLUGINS_EXPORT WizardPage : public QScrollArea
+{
+
+public:
+
+    WizardPage(KAssistantDialog* dlg, const QString& title);
+    virtual ~WizardPage();
+
+    KPageWidgetItem* page() const;
+
+    void setPageWidget(QWidget* w);
+    void setLeftBottomPix(const QPixmap& pix);
+
+private:
+
+    WizardPagePriv* const d;
+};
+
+}   // namespace KIPIPlugins
+
+#endif /* WIZARD_PAGE_H */
