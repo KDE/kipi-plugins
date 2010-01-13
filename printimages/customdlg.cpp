fb54a138cbfcc5540e017ba2e9bf42f9d2977c00
Angelo Naselli <anaselli@linux.it>
2010-01-13 22:36:12 +0000

    Added tool tips and whatis strings GUI:
    svn path=/trunk/extragear/graphics/kipi-plugins/; revision=1074357

--- printimages/customdlg.cpp
+++ printimages/customdlg.cpp
@@ -36,6 +36,22 @@ CustomLayoutDlg::CustomLayoutDlg ( QWidget *parent )
 
   connect ( m_doneButton, SIGNAL ( clicked() ),
             this, SLOT ( accept() ) );
+            
+  m_photoGridCheck->setToolTip(i18n("Choose your grid size"));
+  m_photoGridCheck->setWhatsThis(i18n("Choose your grid size"));
+  m_gridRows->setToolTip(i18n("Number of rows"));
+  m_gridRows->setWhatsThis(i18n("Insert number of rows"));
+  m_gridColumns->setToolTip(i18n("Number of columns"));
+  m_gridColumns->setWhatsThis(i18n("Insert number of columns"));
+  
+  m_fitAsManyCheck->setToolTip(i18n("Choose to have a custom photo size album"));
+  m_fitAsManyCheck->setWhatsThis(i18n("Choose to have a custom photo size album"));
+  m_photoHeight->setToolTip(i18n("Photo height"));
+  m_photoHeight->setWhatsThis(i18n("Insert photo height"));
+  m_photoWidth->setToolTip(i18n("Photo width"));
+  m_photoWidth->setWhatsThis(i18n("Insert photo width"));
+  
+  m_autorotate->setToolTip(i18n("Auto rotate photo"));  
 }
 
 CustomLayoutDlg:: ~CustomLayoutDlg()
