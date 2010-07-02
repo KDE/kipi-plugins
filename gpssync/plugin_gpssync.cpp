6afd297b30d49e67998019a44b66b1a9df398746
Gilles Caulier <caulier.gilles@gmail.com>
2010-07-02 12:33:56 +0000

    simplify menu entry. Just on item to add
    svn path=/branches/extragear/graphics/kipi-plugins/; revision=1145289

--- gpssync/plugin_gpssync.cpp
+++ gpssync/plugin_gpssync.cpp
@@ -7,7 +7,7 @@
  * Description : a plugin to synchronize pictures with
  *               a GPS device.
  *
- * Copyright (C) 2006-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2010 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -22,7 +22,6 @@
  *
  * ============================================================ */
 
-#include "plugin_gpssync.h"
 #include "plugin_gpssync.moc"
 
 // Qt includes
@@ -59,7 +58,7 @@
 K_PLUGIN_FACTORY( GPSSyncFactory, registerPlugin<Plugin_GPSSync>(); )
 K_EXPORT_PLUGIN ( GPSSyncFactory("kipiplugin_gpssync") )
 
-Plugin_GPSSync::Plugin_GPSSync(QObject *parent, const QVariantList &)
+Plugin_GPSSync::Plugin_GPSSync(QObject* parent, const QVariantList&)
               : KIPI::Plugin( GPSSyncFactory::componentData(), parent, "GPSSync")
 {
     kDebug(AREA_CODE_LOADING) << "Plugin_GPSSync plugin loaded" ;
@@ -69,16 +68,14 @@ void Plugin_GPSSync::setup( QWidget* widget )
 {
     KIPI::Plugin::setup( widget );
 
-    m_action_geolocation = new KActionMenu(KIcon("applications-internet"), i18n("Geolocation"), actionCollection());
+    m_action_geolocation = actionCollection()->addAction("gpssync");
+    m_action_geolocation->setText(i18n("Geo-location"));
+    m_action_geolocation->setIcon(KIcon("applications-internet"));
 
-    KAction *gpssync = actionCollection()->addAction("gpssync");
-    gpssync->setText(i18n("Correlator"));
-    gpssync->setIcon(KIcon("gpsimagetag"));
-    connect(gpssync, SIGNAL(triggered(bool)),
+    connect(m_action_geolocation, SIGNAL(triggered(bool)),
             this, SLOT(slotGPSSync()));
-    m_action_geolocation->addAction(gpssync);
 
-    addAction( m_action_geolocation );
+    addAction(m_action_geolocation);
 
     m_interface = dynamic_cast< KIPI::Interface* >( parent() );
 
@@ -91,25 +88,18 @@ void Plugin_GPSSync::setup( QWidget* widget )
     KIPI::ImageCollection selection = m_interface->currentSelection();
     m_action_geolocation->setEnabled( selection.isValid() && !selection.images().isEmpty() );
 
-    connect( m_interface, SIGNAL(selectionChanged(bool)),
-             m_action_geolocation, SLOT(setEnabled(bool)));
+    connect(m_interface, SIGNAL(selectionChanged(bool)),
+            m_action_geolocation, SLOT(setEnabled(bool)));
 }
 
 void Plugin_GPSSync::slotGPSSync()
 {
-    kDebug()<<1;
     KIPI::ImageCollection images = m_interface->currentSelection();
 
     if ( !images.isValid() || images.images().isEmpty() )
         return;
 
-    /* NOTE: this plugin do not use yet GPSBabel to convert GPS data file to GPX
-    QString gpsBabelVersion;
-    if (!checkBinaries(gpsBabelVersion))
-        return;
-    */
-
-    KIPIGPSSyncPlugin::GPSSyncDialog *dialog = new KIPIGPSSyncPlugin::GPSSyncDialog(
+    KIPIGPSSyncPlugin::GPSSyncDialog* dialog = new KIPIGPSSyncPlugin::GPSSyncDialog(
                                                m_interface, kapp->activeWindow());
 
     dialog->setImages( images.images() );
@@ -120,8 +110,6 @@ KIPI::Category Plugin_GPSSync::category( KAction* action ) const
 {
     if ( action == m_action_geolocation )
        return KIPI::ImagesPlugin;
-    if ( action == m_actionKMLExport )
-       return KIPI::ExportPlugin;
 
     kWarning() << "Unrecognized action for plugin category identification" ;
     return KIPI::ImagesPlugin; // no warning from compiler, please
--- gpssync/plugin_gpssync.h
+++ gpssync/plugin_gpssync.h
@@ -7,7 +7,7 @@
  * Description : a plugin to synchronize pictures with
  *               a GPS device.
  *
- * Copyright 2006-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright 2006-2010 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -33,13 +33,12 @@
 
 #include <libkipi/plugin.h>
 
-class KActionMenu;
 class KAction;
 
 namespace KIPI
 {
 class Interface;
-} // namespace KIPI
+}
 
 class Plugin_GPSSync : public KIPI::Plugin
 {
@@ -47,10 +46,10 @@ class Plugin_GPSSync : public KIPI::Plugin
 
 public:
 
-    Plugin_GPSSync(QObject *parent, const QVariantList &args);
+    Plugin_GPSSync(QObject* parent, const QVariantList& args);
 
-    KIPI::Category category( KAction* action ) const;
-    void setup( QWidget* );
+    KIPI::Category category(KAction* action) const;
+    void setup(QWidget*);
 
 protected Q_SLOTS:
 
@@ -58,12 +57,7 @@ protected Q_SLOTS:
 
 private:
 
-    bool checkBinaries(QString &gpsBabelVersion);
-
-private:
-
-    KActionMenu     *m_action_geolocation;
-    KAction         *m_actionKMLExport;
+    KAction         *m_action_geolocation;
 
     KIPI::Interface *m_interface;
 };
