7b1de61ce48254905394ac0a0401a3e2e30ffbfc
Gilles Caulier <caulier.gilles@gmail.com>
2011-04-05 10:53:48 +0200

    polish

--- metadataedit/xmp/xmpsubjects.cpp
+++ metadataedit/xmp/xmpsubjects.cpp
@@ -6,7 +6,7 @@
  * Date        : 2006-10-15
  * Description : XMP subjects settings page.
  *
- * Copyright (C) 2006-2010 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2009      by Andi Clemens <andi dot clemens at gmx dot net>
  *
  * This program is free software; you can redistribute it
@@ -46,7 +46,7 @@ XMPSubjects::XMPSubjects(QWidget* parent)
     // - ':' (\x3A)
     // - '?' (\x3F)
     QRegExp subjectRx("[^*:?]+$");
-    QValidator *subjectValidator = new QRegExpValidator(subjectRx, this);
+    QValidator* subjectValidator = new QRegExpValidator(subjectRx, this);
 
     // --------------------------------------------------------
 
@@ -59,28 +59,28 @@ XMPSubjects::XMPSubjects(QWidget* parent)
                                   "Code is used."));
 
     m_refEdit->setWhatsThis(i18n("Enter here the Subject Reference Number. "
-                                  "Provides a numeric code to indicate the Subject Name plus "
-                                  "optional Subject Matter and Subject Detail Names in the "
-                                  "language of the service. Subject Reference is a number "
-                                  "from the range 01000000 to 17999999 and represent a "
-                                  "language independent international reference to "
-                                  "a Subject. A Subject is identified by its Reference Number "
-                                  "and corresponding Names taken from a standard lists given "
-                                  "by XMP/NAA. If a standard reference code is used, these lists "
-                                  "are the English language reference versions. "
-                                  "This field is limited to 8 digit code."));
+                                 "Provides a numeric code to indicate the Subject Name plus "
+                                 "optional Subject Matter and Subject Detail Names in the "
+                                 "language of the service. Subject Reference is a number "
+                                 "from the range 01000000 to 17999999 and represent a "
+                                 "language independent international reference to "
+                                 "a Subject. A Subject is identified by its Reference Number "
+                                 "and corresponding Names taken from a standard lists given "
+                                 "by XMP/NAA. If a standard reference code is used, these lists "
+                                 "are the English language reference versions. "
+                                 "This field is limited to 8 digit code."));
 
     m_nameEdit->setValidator(subjectValidator);
     m_nameEdit->setWhatsThis(i18n("Enter here the Subject Name. English language is used "
-                                   "if you selected a standard XMP/NAA reference code."));
+                                  "if you selected a standard XMP/NAA reference code."));
 
     m_matterEdit->setValidator(subjectValidator);
     m_matterEdit->setWhatsThis(i18n("Enter here the Subject Matter Name. English language is used "
-                                     "if you selected a standard XMP/NAA reference code."));
+                                    "if you selected a standard XMP/NAA reference code."));
 
     m_detailEdit->setValidator(subjectValidator);
     m_detailEdit->setWhatsThis(i18n("Enter here the Subject Detail Name. English language is used "
-                                     "if you selected a standard XMP/NAA reference code."));
+                                    "if you selected a standard XMP/NAA reference code."));
 
     // reset the note label, not used in XMP view
     delete m_note;
