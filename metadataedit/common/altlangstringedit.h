5028bff14ec6159914c90ba6bf81a1412a9fd677
Gilles Caulier <caulier.gilles@gmail.com>
2011-04-05 09:00:05 +0200

    port this widget to KEXiv2 to reduce duplicate code. more private internal container. Code polish CCMAIL: victor.dodon@cti.pub.ro

--- metadataedit/common/altlangstringedit.h
+++ metadataedit/common/altlangstringedit.h
@@ -7,7 +7,7 @@
  * Description : a widget to edit a tag with multiple alternative
  *               language string entries.
  *
- * Copyright (C) 2007-2010 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2007-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -37,8 +37,6 @@ using namespace KExiv2Iface;
 namespace KIPIMetadataEditPlugin
 {
 
-class AltLangStringsEditPriv;
-
 class AltLangStringsEdit : public QWidget
 {
     Q_OBJECT
@@ -65,17 +63,11 @@ Q_SIGNALS:
 
 private Q_SLOTS:
 
-    void slotSelectionChanged();
-    void slotAddValue();
-    void slotDeleteValue();
-    void slotReplaceValue();
+    void slotSelectionChanged(const QString& lang);
 
 private:
 
-    void loadLangAltListEntries();
-
-private:
-
+    class AltLangStringsEditPriv;
     AltLangStringsEditPriv* const d;
 };
 
