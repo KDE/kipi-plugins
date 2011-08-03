94ce36dc54f440ac8c7a4be3d1cd7ec521ce8299
Alexander Potashev <aspotashev@gmail.com>
2011-08-03 14:12:52 +0400

    Fix memory corruption introduced in cf1cc0b5bb356d7ce6a864237852cd4191ba833d
    BUG:279078

--- yandexfotki/yandexrsa.cpp
+++ yandexfotki/yandexrsa.cpp
@@ -1049,7 +1049,7 @@ void CCryptoProviderRSA::Encrypt(const char* inbuf, size_t in_size,char* outbuf,
     // hence -1
     int portion_len = (prkface.m.bits() - 1)  / 8;
     char* prev_crypted = new char[portion_len];
-    memset(&prev_crypted, 0, portion_len);
+    memset(prev_crypted, 0, portion_len);
 
     out_size = 0;
 

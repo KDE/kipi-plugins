KDE/kipi-plugins

commit cf1cc0b5bb356d7ce6a864237852cd4191ba833d

use new/delete allocation here, because MSVC 2008 don't like char[] array allocation with a variable size... CCMAIL: roman@tsisyk.com

yandexfotki/yandexrsa.cpp
@@ -1048,7 +1048,7 @@ void CCryptoProviderRSA::Encrypt(const char* inbuf, size_t in_size,char* outbuf,
     // must ensure that any data block would be < key's modulus
     // hence -1
     int portion_len = (prkface.m.bits() - 1)  / 8;
-    char prev_crypted[portion_len];
+    char* prev_crypted = new char[portion_len];
     memset(&prev_crypted, 0, portion_len);
 
     out_size = 0;
@@ -1082,6 +1082,7 @@ void CCryptoProviderRSA::Encrypt(const char* inbuf, size_t in_size,char* outbuf,
         in_size-=cur_size;
     }
 
+    delete [] prev_crypted;
     return;
 }
 

commit 94ce36dc54f440ac8c7a4be3d1cd7ec521ce8299

Fix memory corruption introduced in cf1cc0b5bb356d7ce6a864237852cd4191ba833d
BUG:279078

yandexfotki/yandexrsa.cpp
@@ -1049,7 +1049,7 @@ void CCryptoProviderRSA::Encrypt(const char* inbuf, size_t in_size,char* outbuf,
     // hence -1
     int portion_len = (prkface.m.bits() - 1)  / 8;
     char* prev_crypted = new char[portion_len];
-    memset(&prev_crypted, 0, portion_len);
+    memset(prev_crypted, 0, portion_len);
 
     out_size = 0;
 