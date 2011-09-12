0e604c925d7c5ffd9bf31355e777c64a15ae1ec8
Gilles Caulier <caulier.gilles@gmail.com>
2011-09-12 18:16:30 +0200

    set aboutdata properties.

--- photolayoutseditor/main.cpp
+++ photolayoutseditor/main.cpp
@@ -1,34 +1,61 @@
+/* ============================================================
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2011-09-01
+ * Description : a plugin to create photo layouts by fusion of several images.
+ * Acknowledge : based on the expoblending plugin
+ *
+ * Copyright (C) 2011 by Łukasz Spas <lukasz dot spas at gmail dot com>
+ * Copyright (C) 2009-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
 #include "photolayoutseditor.h"
 #include "PLEAboutData.h"
 
 #include <QDesktopWidget>
 #include <QResource>
-#include <qmath.h>
 #include <QString>
 
 #include <klocalizedstring.h>
 #include <kapplication.h>
 #include <kcmdlineargs.h>
 #include <kaboutdata.h>
+#include <kicon.h>
 
 using namespace KIPIPhotoLayoutsEditor;
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-
     PLEAboutData aboutData;
+    aboutData.setAppName("photolayoutseditor");
+    aboutData.setCatalogName("kipiplugin_photolayoutseditor");
 
     KCmdLineArgs::init(argc,argv,&aboutData);
     KCmdLineOptions options;
     options.add("+file", ki18n("Input file"));
     KCmdLineArgs::addCmdLineOptions(options);
 
-    KApplication a;
+    KApplication app;
+    aboutData.setProgramLogo(KIcon("photolayoutseditor"));
 
-    PhotoLayoutsEditor * w = PhotoLayoutsEditor::instance(0);
+    PhotoLayoutsEditor* w = PhotoLayoutsEditor::instance(0);
     w->setAttribute(Qt::WA_DeleteOnClose, true);
 
-    KCmdLineArgs * args = KCmdLineArgs::parsedArgs();
+    KCmdLineArgs* args = KCmdLineArgs::parsedArgs();
     if (args->count())
     {
         KUrl url = args->url(0);
@@ -38,7 +65,7 @@ int main(int argc, char *argv[])
 
     w->show();
 
-    int result = a.exec();
+    int result = app.exec();
 
     return result;
 }
