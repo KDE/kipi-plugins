3eefee763a98841ca0f2f9a6c8552edd337149f3
Albert Astals Cid <tsdgeos@terra.es>
2011-09-22 00:39:30 +0200

    Add no-c-format as hinted by Yuri

--- photolayoutseditor/widgets/canvas/CropWidgetItem.cpp
+++ photolayoutseditor/widgets/canvas/CropWidgetItem.cpp
@@ -256,6 +256,8 @@ void CropWidgetItem::keyPressEvent(QKeyEvent * event)
         else
         {
             KMessageBox::error(0,
+            // We need this hint to xgettext otherwise it thinks that %1p is the printf %p with a 1 modifier
+            // xgettext: no-c-format
                                i18n("Bounding rectangle of the crop shape has size [%1px x %2px] "
                                     "and it's less than 1px x 1px",
                                     QString::number(qRound(d->m_rect.width())),
