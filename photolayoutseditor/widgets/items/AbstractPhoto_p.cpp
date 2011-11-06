d41df130e6f18cfdcc42a49b435d0eec77c5f7be
Gilles Caulier <caulier.gilles@gmail.com>
2011-11-06 14:42:45 +0100

    missing header polish

--- photolayoutseditor/widgets/items/AbstractPhoto_p.cpp
+++ photolayoutseditor/widgets/items/AbstractPhoto_p.cpp
@@ -28,12 +28,13 @@
 
 using namespace KIPIPhotoLayoutsEditor;
 
-AbstractPhotoPrivate::AbstractPhotoPrivate(AbstractPhoto * item) :
+AbstractPhotoPrivate::AbstractPhotoPrivate(AbstractPhoto* item) :
     m_item(item),
     m_visible(true)
-{}
+{
+}
 
-void AbstractPhotoPrivate::setCropShape(const QPainterPath & cropShape)
+void AbstractPhotoPrivate::setCropShape(const QPainterPath& cropShape)
 {
     m_crop_shape = cropShape;
     m_item->refresh();
@@ -44,7 +45,7 @@ QPainterPath & AbstractPhotoPrivate::cropShape()
     return m_crop_shape;
 }
 
-void AbstractPhotoPrivate::setName(const QString & name)
+void AbstractPhotoPrivate::setName(const QString& name)
 {
     if (name.isEmpty())
         return;
--- photolayoutseditor/widgets/items/AbstractPhoto_p.h
+++ photolayoutseditor/widgets/items/AbstractPhoto_p.h
@@ -1,3 +1,28 @@
+/* ============================================================
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2011-09-01
+ * Description : a plugin to create photo layouts by fusion of several images.
+ * Acknowledge : based on the expoblending plugin
+ *
+ * Copyright (C) 2011 by Łukasz Spas <lukasz dot spas at gmail dot com>
+ * Copyright (C) 2009-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
 #ifndef ABSTRACTPHOTO_P_H
 #define ABSTRACTPHOTO_P_H
 
@@ -12,35 +37,39 @@ namespace KIPIPhotoLayoutsEditor
     class AbstractPhoto;
     class AbstractPhotoPrivate
     {
-        AbstractPhoto * m_item;
+        public:
 
-        AbstractPhotoPrivate(AbstractPhoto * item);
+            AbstractPhotoPrivate(AbstractPhoto* item);
 
-        // Crop shape
-        void setCropShape(const QPainterPath & cropShape);
-        QPainterPath & cropShape();
-        QPainterPath m_crop_shape;
+            // Crop shape
+            void setCropShape(const QPainterPath& cropShape);
+            QPainterPath& cropShape();
 
-        void setName(const QString & name);
-        QString name();
-        QString m_name;
+            void setName(const QString& name);
+            QString name();
 
-        // For loading purpose only
-        bool m_visible;
-        QPointF m_pos;
-        QTransform m_transform;
+        public:
 
-        mutable QString m_id;
-        PhotoEffectsGroup * m_effects_group;
-        BordersGroup * m_borders_group;
+            QPainterPath       m_crop_shape;
+            AbstractPhoto*     m_item;
+            QString            m_name;
 
-        // Icon object
-        QIcon m_icon;
+            // For loading purpose only
+            bool               m_visible;
+            QPointF            m_pos;
+            QTransform         m_transform;
 
-        friend class AbstractPhoto;
-        friend class AbstractPhotoItemLoader;
-        friend class CropShapeChangeCommand;
-        friend class ItemNameChangeCommand;
+            mutable QString    m_id;
+            PhotoEffectsGroup* m_effects_group;
+            BordersGroup*      m_borders_group;
+
+            // Icon object
+            QIcon              m_icon;
+
+            friend class AbstractPhoto;
+            friend class AbstractPhotoItemLoader;
+            friend class CropShapeChangeCommand;
+            friend class ItemNameChangeCommand;
     };
 }
 
