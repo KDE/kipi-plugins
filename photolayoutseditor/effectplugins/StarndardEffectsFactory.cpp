44ebc1e5a0783c09adec3c8b27b3f8d82338eb90
Gilles Caulier <caulier.gilles@gmail.com>
2011-11-06 14:25:10 +0100

    missing header

--- photolayoutseditor/effectplugins/StarndardEffectsFactory.cpp
+++ photolayoutseditor/effectplugins/StarndardEffectsFactory.cpp
@@ -1,3 +1,28 @@
+/* ============================================================
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2011-09-01
+ * Description : a plugin to create photo layouts by fusion of several images.
+ * Acknowledge : based on the expoblending plugin
+ *
+ * Copyright (C) 2011 by Łukasz Spas <lukasz dot spas at gmail dot com>
+ * Copyright (C) 2009-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
 #include "StarndardEffectsFactory.h"
 
 #include "BlurPhotoEffect.h"
@@ -7,11 +32,11 @@
 
 using namespace KIPIPhotoLayoutsEditor;
 
-StarndardEffectsFactory::StarndardEffectsFactory(QObject * parent) :
+StarndardEffectsFactory::StarndardEffectsFactory(QObject* parent) :
     AbstractPhotoEffectFactory(parent)
 {}
 
-AbstractPhotoEffectInterface * StarndardEffectsFactory::getEffectInstance(const QString & name)
+AbstractPhotoEffectInterface * StarndardEffectsFactory::getEffectInstance(const QString& name)
 {
     if (name == i18n("Blur effect"))
         return new BlurPhotoEffect(this);
@@ -27,7 +52,7 @@ AbstractPhotoEffectInterface * StarndardEffectsFactory::getEffectInstance(const 
 QString StarndardEffectsFactory::effectName() const
 {
     return i18n("Blur effect") + QString(";") +
-            i18n("Colorize effect") + QString(";") +
-            i18n("Grayscale effect") + QString(";") +
-            i18n("Sepia effect");
+           i18n("Colorize effect") + QString(";") +
+           i18n("Grayscale effect") + QString(";") +
+           i18n("Sepia effect");
 }
