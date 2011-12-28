3990e79399a48a2f811516f148d014ffbd2ee353
Gilles Caulier <caulier.gilles@gmail.com>
2011-12-29 00:24:31 +0100

    polish

--- tests/multithreading/actionthread.cpp
+++ tests/multithreading/actionthread.cpp
@@ -6,7 +6,7 @@
  * Date        : 2011-12-28
  * Description : re-implementation of action thread using threadweaver
  *
- * Copyright (C) 2012      by Janardhan Reddy <annapareddyjanardhanreddy at gmail dot com>
+ * Copyright (C) 2011-2012 by A Janardhan Reddy <annapareddyjanardhanreddy at gmail dot com>
  * Copyright (C) 2011-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
