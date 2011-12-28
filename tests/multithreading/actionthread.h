91dbda13623caf0d60d3bb8d5153b61b05303cbc
Gilles Caulier <caulier.gilles@gmail.com>
2011-12-29 00:15:26 +0100

    add copyright

--- tests/multithreading/actionthread.h
+++ tests/multithreading/actionthread.h
@@ -6,7 +6,8 @@
  * Date        : 2011-12-28
  * Description : re-implementation of action thread using threadweaver
  *
- * Copyright (C) 2011-2012
+ * Copyright (C) 2012      by Janardhan Reddy <annapareddyjanardhanreddy at gmail dot com>
+ * Copyright (C) 2011-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
