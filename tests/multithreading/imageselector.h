26ead2fef6b0a73bb014c123eea249774a0c44bc
Gilles Caulier <caulier.gilles@gmail.com>
2012-01-01 20:25:33 +0100

    (c) and anti-spam

--- tests/multithreading/imageselector.h
+++ tests/multithreading/imageselector.h
@@ -6,7 +6,8 @@
  * Date        : 2011-12-28
  * Description : Simple gui to select images
  *
- * Copyright (C) 2011-2012 by A Janardhan Reddy <annapareddyjanardhanreddy@gmail.com>
+ * Copyright (C) 2011-2012 by A Janardhan Reddy <annapareddyjanardhanreddy at gmail dot com>
+ * Copyright (C) 2011-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
