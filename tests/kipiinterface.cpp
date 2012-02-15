26b012ba9af99fa6e4c882f1e95b9c5542ae792e
Gilles Caulier <caulier.gilles@gmail.com>
2012-02-15 10:07:24 +0100

    add exemple to use kipi tool interface

--- tests/kipiinterface.cpp
+++ tests/kipiinterface.cpp
@@ -9,6 +9,8 @@
  *
  * @author Copyright (C) 2009-2010 by Michael G. Hansen
  *         <a href="mailto:mike at mghansen dot de">mike at mghansen dot de</a>
+ * @author Copyright (C) 2011-2012 by Gilles Caulier
+ *         <a href="mailto:caulier dot gilles at gmail dot com">caulier dot gilles at gmail dot com</a>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
