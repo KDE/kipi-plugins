7227946e48495c1eedbe94ebdecdf53c6fc315d4
Gilles Caulier <caulier.gilles@gmail.com>
2012-02-21 15:50:06 +0100

    bug #211758 is closed. This line can be uncommented now

--- common/libkipiplugins/tools/kpwriteimage.cpp
+++ common/libkipiplugins/tools/kpwriteimage.cpp
@@ -7,7 +7,7 @@
  * Description : Kipi-Plugins shared library.
  *               Interface to write image data to common picture format.
  *
- * Copyright (C) 2007-2010 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2007-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -768,9 +768,7 @@ bool KPWriteImage::write2TIFF(const QString& destPath)
     TIFFClose(tif);
 
     // Store metadata (Exiv2 0.18 support tiff writing mode)
-
-    // See B.K.O #211758: disable tiff metadata update until we found where is the problem exactly.
-    //d->metadata.save(destPath);
+    d->metadata.save(destPath);
 
     return true;
 }
--- common/libkipiplugins/tools/kpwriteimage.h
+++ common/libkipiplugins/tools/kpwriteimage.h
@@ -7,7 +7,7 @@
  * Description : Kipi-Plugins shared library.
  *               Interface to write image data to common picture format.
  *
- * Copyright (C) 2007-2010 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2007-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
