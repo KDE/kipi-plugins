62ffbc405f40d344253db3b4ddd8c22172656729
Benjamin Girault <benjamin.girault@gmail.com>
2012-03-18 23:42:09 +0100

    Panorama: CPFind detection with RegExp

--- panorama/manager/cpfindbinary.cpp
+++ panorama/manager/cpfindbinary.cpp
@@ -6,7 +6,7 @@
  * Date        : 2011-05-23
  * Description : Autodetects cpfind binary program and version
  *
- * Copyright (C) 2011 by Benjamin Girault <benjamin dot girault at gmail dot com>
+ * Copyright (C) 2011-2012 by Benjamin Girault <benjamin dot girault at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -22,64 +22,38 @@
 
 #include "cpfindbinary.h"
 
-// Qt includes
-
-#include <QProcess>
-#include <QRegExp>
-
 // KDE includes
 
 #include <kdebug.h>
-#include <kglobal.h>
-#include <kstandarddirs.h>
 
 namespace KIPIPanoramaPlugin
 {
 
 bool CPFindBinary::parseHeader(const QString& output)
 {
-    bool ret = false;
-
-    // FIXME: Change that to a regexp
-    QString headerStartsNew("Hugin's cpfind ");          // For Hugin 2011.4
     QStringList lines    = output.split('\n');
     m_developmentVersion = false;
-    
+
     foreach(QString line, lines)
     {
         kDebug() << path() << " help header line: \n" << line;
 
-        if (line.startsWith(m_headerStarts))
+        if (line.contains(headerRegExp))
         {
-            m_version = line.remove(0, m_headerStarts.length()).section('.', 0, 1);
-        }
-        else if (line.startsWith(headerStartsNew))
-        {
-            m_version = line.remove(0, headerStartsNew.length()).section('.', 0, 1);
-        }
-        else
-        {
-            kDebug() << "no match";
-        }
+            m_version = headerRegExp.cap(2);
 
-        if (!m_version.isEmpty())
-        {
-            if (m_version.startsWith("Pre-Release "))
+            if (headerRegExp.cap(1) != QString())
             {
                 m_developmentVersion = true;
-                m_version.remove("Pre-Release ");            // Special case with Hugin beta.
             }
-            ret = true;
-            break;
+
+            return true;
         }
-        else
-        {
-            ret = false;
-        }
+
         m_developmentVersion = true;
     }
 
-    return ret;
+    return false;
 }
 
 }  // namespace KIPIPanoramaPlugin
