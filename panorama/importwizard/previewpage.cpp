65148d7700f75b8cabbefde2284a90d912b59107
Benjamin Girault <benjamin.girault@gmail.com>
2012-03-19 00:08:28 +0100

    Adding some copyright info

--- panorama/importwizard/previewpage.cpp
+++ panorama/importwizard/previewpage.cpp
@@ -6,7 +6,7 @@
  * Date        : 2011-05-23
  * Description : a plugin to create panorama by fusion of several images.
  *
- * Copyright (C) 2011 by Benjamin Girault <benjamin dot girault at gmail dot com>
+ * Copyright (C) 2011-2012 by Benjamin Girault <benjamin dot girault at gmail dot com>
  * Copyright (C) 2011-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
