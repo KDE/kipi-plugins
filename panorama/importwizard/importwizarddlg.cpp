57d73635bcad3be7efbc96909401aef1fc097aac
Gilles Caulier <caulier.gilles@gmail.com>
2012-04-26 21:33:20 +0200

    polish

--- panorama/importwizard/importwizarddlg.cpp
+++ panorama/importwizard/importwizarddlg.cpp
@@ -8,7 +8,7 @@
  * Acknowledge : based on the expoblending plugin
  *
  * Copyright (C) 2011-2012 by Benjamin Girault <benjamin dot girault at gmail dot com>
- * Copyright (C) 2009-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2009-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -72,17 +72,17 @@ struct ImportWizardDlg::ImportWizardDlgPriv
     {
     }
 
-    Manager*            mngr;
+    Manager*           mngr;
 
-    IntroPage*          introPage;
-    ItemsPage*          itemsPage;
-    PreProcessingPage*  preProcessingPage;
-    OptimizePage*       optimizePage;
-    PreviewPage*        previewPage;
-    LastPage*           lastPage;
+    IntroPage*         introPage;
+    ItemsPage*         itemsPage;
+    PreProcessingPage* preProcessingPage;
+    OptimizePage*      optimizePage;
+    PreviewPage*       previewPage;
+    LastPage*          lastPage;
 };
 
-ImportWizardDlg::ImportWizardDlg(Manager* mngr, QWidget* parent)
+ImportWizardDlg::ImportWizardDlg(Manager* const mngr, QWidget* const parent)
     : KAssistantDialog(parent), d(new ImportWizardDlgPriv)
 {
     setModal(false);
