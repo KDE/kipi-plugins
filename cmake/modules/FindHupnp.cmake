a8797f463f549e85f4ba2c8f0f058d3286e2aa1a
Gilles Caulier <caulier.gilles@gmail.com>
2012-06-06 11:24:52 +0200

    polish

--- cmake/modules/FindHupnp.cmake
+++ cmake/modules/FindHupnp.cmake
@@ -1,29 +1,29 @@
-# - Find HUpnp
-# HUpnp is a Universal Plug and Play (UPnP) library
+# Find HUpnp - HUpnp is a Universal Plug and Play (UPnP) library
 # used by the UPnP collection.
-# Defines:
+#
+# This module defines
 # HUPNP_INCLUDE_DIR
 # HUPNP_LIBS
 # HUPNP_FOUND
 
-find_path(HUPNP_INCLUDE_DIR
-	HUpnp HINTS
-	${PC_HUPNP_INCLUDE_DIR}
-	/usr/include/HUpnpCore/
-	/usr/include/HUpnpAv/
-)
+FIND_PATH(HUPNP_INCLUDE_DIR
+          HUpnp HINTS
+          ${PC_HUPNP_INCLUDE_DIR}
+          /usr/include/HUpnpCore/
+          /usr/include/HUpnpAv/
+         )
 
-find_library(HUPNP_LIBS HUpnp PATHS ${PC_HUPNP_LIBRARY})
+FIND_LIBRARY(HUPNP_LIBS HUpnp PATHS ${PC_HUPNP_LIBRARY})
 
-if(HUPNP_INCLUDE_DIR AND HUPNP_LIBS)
-  set(HUPNP_FOUND TRUE)
-  message(STATUS "Found HUpnp")
-else(HUPNP_INCLUDE_DIR and HUPNP_LIBS)
-  set(HUPNP_FOUND FALSE)
-  if(HUPNP_FIND_REQUIRED)
-    message(FATAL_ERROR "Could NOT find required package HUpnp: <http://herqq.org>")
-  endif(HUPNP_FIND_REQUIRED)
-endif(HUPNP_INCLUDE_DIR AND HUPNP_LIBS)
+IF(HUPNP_INCLUDE_DIR AND HUPNP_LIBS)
+    SET(HUPNP_FOUND TRUE)
+    MESSAGE(STATUS "Found HUpnp")
+ELSE(HUPNP_INCLUDE_DIR and HUPNP_LIBS)
+    SET(HUPNP_FOUND FALSE)
+    IF(HUPNP_FIND_REQUIRED)
+        MESSAGE(FATAL_ERROR "Could NOT find required package HUpnp: <http://herqq.org>")
+    ENDIF(HUPNP_FIND_REQUIRED)
+ENDIF(HUPNP_INCLUDE_DIR AND HUPNP_LIBS)
 
-include(FindPackageHandleStandardArgs)
-  FIND_PACKAGE_HANDLE_STANDARD_ARGS(Hupnp DEFAULT_MSG HUPNP_INCLUDE_DIR HUPNP_LIBS )
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Hupnp DEFAULT_MSG HUPNP_INCLUDE_DIR HUPNP_LIBS)
