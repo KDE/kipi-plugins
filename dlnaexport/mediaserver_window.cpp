6ec5e8cca4d01447ba5d620a64e92e745dd6049c
Gilles Caulier <caulier.gilles@gmail.com>
2012-06-12 07:38:35 +0200

    use plugin namespace wrapping use hupnp namespace fix header polish

--- dlnaexport/mediaserver_window.cpp
+++ dlnaexport/mediaserver_window.cpp
@@ -1,52 +1,51 @@
-/*
- *  Copyright (C) 2011 Tuomo Penttinen, all rights reserved.
+/* ============================================================
  *
- *  Author: Tuomo Penttinen <tp@herqq.org>
+ * This file is a part of kipi-plugins project
+ * http://www.digikam.org
  *
- *  This file is part of an application named HUpnpAvSimpleTestApp
- *  used for demonstrating how to use the Herqq UPnP A/V (HUPnPAv) library.
+ * Date        : 2012-05-28
+ * Description : a KIPI plugin to export pics through DLNA technology.
  *
- *  HUpnpAvSimpleTestApp is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * Copyright (C) 2012 by Smit Mehta <smit dot meh at gmail dot com>
+ * Copyright (C) 2011 Tuomo Penttinen <tp at herqq dot org>
  *
- *  HUpnpAvSimpleTestApp is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU General Public License for more details.
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with HUpnpAvSimpleTestApp. If not, see <http://www.gnu.org/licenses/>.
- */
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
 
 #include "mediaserver_window.moc"
+
+// KDE includes
 
 #include <kstandarddirs.h>
 #include <kdebug.h>
 
+// libHUpnp includes
+
 #include <HUpnpCore/HUpnp>
 #include <HUpnpCore/HDeviceInfo>
 #include <HUpnpCore/HDeviceHostConfiguration>
-
 #include <HUpnpAv/HRootDir>
 #include <HUpnpAv/HAvDeviceModelCreator>
 #include <HUpnpAv/HMediaServerDeviceConfiguration>
-
 #include <HUpnpAv/HFileSystemDataSource>
 #include <HUpnpAv/HContentDirectoryServiceConfiguration>
 
-using namespace Herqq::Upnp;
-using namespace Herqq::Upnp::Av;
-
-/*******************************************************************************
- * MediaServerWindow
- *******************************************************************************/
-MediaServerWindow::MediaServerWindow( QObject* parent )
-    : QObject(parent),
-      m_datasource( 0 )
+namespace KIPIDLNAExportPlugin
 {
 
+MediaServerWindow::MediaServerWindow(QObject* const parent)
+    : QObject(parent),
+      m_datasource(0)
+{
     // 1) Configure a data source
     HFileSystemDataSourceConfiguration datasourceConfig;
     // Here you could configure the data source in more detail if needed. For example,
@@ -110,3 +109,5 @@ void MediaServerWindow::on_addContentButton_clicked(QString dirName, bool mode)
         m_datasource->add(rd);
     }
 }
+
+} // namespace KIPIDLNAExportPlugin
--- dlnaexport/mediaserver_window.h
+++ dlnaexport/mediaserver_window.h
@@ -1,30 +1,44 @@
-/*
- *  Copyright (C) 2011 Tuomo Penttinen, all rights reserved.
+/* ============================================================
  *
- *  Author: Tuomo Penttinen <tp@herqq.org>
+ * This file is a part of kipi-plugins project
+ * http://www.digikam.org
  *
- *  This file is part of an application named HUpnpAvSimpleTestApp
- *  used for demonstrating how to use the Herqq UPnP A/V (HUPnPAv) library.
+ * Date        : 2012-05-28
+ * Description : a KIPI plugin to export pics through DLNA technology.
  *
- *  HUpnpAvSimpleTestApp is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * Copyright (C) 2012 by Smit Mehta <smit dot meh at gmail dot com>
+ * Copyright (C) 2011 Tuomo Penttinen <tp at herqq dot org>
  *
- *  HUpnpAvSimpleTestApp is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU General Public License for more details.
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with HUpnpAvSimpleTestApp. If not, see <http://www.gnu.org/licenses/>.
- */
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
 
 #ifndef MEDIASERVER_WINDOW_H
 #define MEDIASERVER_WINDOW_H
 
+// Qt includes
+
+#include <QObject>
+#include <QString>
+
+// libHUpnp includes
+
 #include <HUpnpAv/HUpnpAv>
 #include <HUpnpCore/HDeviceHost>
+
+using namespace Herqq::Upnp;
+using namespace Herqq::Upnp::Av;
+
+namespace KIPIDLNAExportPlugin
+{
 
 class MediaServerWindow : public QObject
 {
@@ -32,15 +46,17 @@ class MediaServerWindow : public QObject
 
 public:
 
-    MediaServerWindow(QObject* parent = 0);
+    MediaServerWindow(QObject* const parent=0);
     virtual ~MediaServerWindow();
 
     void on_addContentButton_clicked(QString, bool);
 
 private:
 
-    Herqq::Upnp::HDeviceHost*               m_deviceHost;
-    Herqq::Upnp::Av::HFileSystemDataSource* m_datasource;
+    HDeviceHost*           m_deviceHost;
+    HFileSystemDataSource* m_datasource;
 };
+
+} // namespace KIPIDLNAExportPlugin
 
 #endif // MEDIASERVER_WINDOW_H
