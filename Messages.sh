fb5114d2c800d0443253417e2bf80bf24faae3fe
Gilles Caulier <caulier.gilles@gmail.com>
2012-06-13 23:16:02 +0200

    fix strings extraction

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,4 @@
 #! /bin/sh
 (cd common/data && $PREPARETIPS > ../../tips.cpp)
-$PREPARETIPS >> tips.cpp
 $XGETTEXT tips.cpp `find common/libkipiplugins -name \*.cpp -o -name \*.h -o -name \*.h.cmake` -o $podir/kipiplugins.pot
 rm -f tips.cpp
