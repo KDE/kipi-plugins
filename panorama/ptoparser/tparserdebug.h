f1de99c3abdb0c2a21f2e3648ff4b93cad8e6da3
Benjamin Girault <benjamin.girault@gmail.com>
2012-10-27 11:22:53 +0200

    Panorama: License header fix

--- panorama/ptoparser/tparserdebug.h
+++ panorama/ptoparser/tparserdebug.h
@@ -1,28 +1,26 @@
-/*
- *  tparserdebug.h
+/*============================================================
+ * 
+ * This file is a part of kipi-plugins project
+ * http://www.digikam.org
  *
- *  Copyright  Daniel M. German
- *  
- *  April 2007
+ * Description : Hugin parser debug header
  *
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU General Public
- *  License as published by the Free Software Foundation; either
- *  version 2 of the License, or (at your option) any later version.
+ * Copyright (C) 2007 Daniel M German <dmgerman at uvic doooot ca>
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- *  This software is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public
- *  License along with this software; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- *
- *  Author: Daniel M German dmgerman at uvic doooot ca
- *
- */
+ * ============================================================ */
 
 #ifndef TPARSERDEBUG_H
 #define TPARSERDEBUG_H
--- panorama/ptoparser/tparsergetters.h
+++ panorama/ptoparser/tparsergetters.h
@@ -1,24 +1,27 @@
-/*
- *  tparsergetters.h
+/*============================================================
+ * 
+ * This file is a part of kipi-plugins project
+ * http://www.digikam.org
  *
- *  Copyright (c) April 2007 Daniel M. German <dmgerman at uvic doooot ca>
- *  Copyright (c) 2012 Benjamin Girault <benjamin dot girault at gmail dot com>
+ * Description : Hugin parser API
  *
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU General Public
- *  License as published by the Free Software Foundation; either
- *  version 2 of the License, or (at your option) any later version.
+ * Copyright (C) 2007 Daniel M German <dmgerman at uvic doooot ca>
+ * Copyright (C) 2012 by Benjamin Girault <benjamin dot girault at gmail dot com>
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- *  This software is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public
- *  License along with this software; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- */
+ * ============================================================ */
 
 #ifndef TPARSERGETTERS_H
 #define TPARSERGETTERS_H
