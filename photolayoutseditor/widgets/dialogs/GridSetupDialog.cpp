34fc6770c0fba6c6980310eeb7ad106e3a7135bd
Gilles Caulier <caulier.gilles@gmail.com>
2013-02-06 14:08:23 +0100

    Coverity Reports fixes

--- photolayoutseditor/widgets/dialogs/GridSetupDialog.cpp
+++ photolayoutseditor/widgets/dialogs/GridSetupDialog.cpp
@@ -7,7 +7,7 @@
  * Description : a plugin to create photo layouts by fusion of several images.
  * Acknowledge : based on the expoblending plugin
  *
- * Copyright (C) 2011 by Łukasz Spas <lukasz dot spas at gmail dot com>
+ * Copyright (C) 2011      by Łukasz Spas <lukasz dot spas at gmail dot com>
  * Copyright (C) 2009-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
@@ -48,11 +48,13 @@ GridSetupDialog::GridSetupDialog(QWidget * parent) :
 
     x = new QDoubleSpinBox(centralWidget);
     KConfigSkeletonItem * hgi = skeleton->findItem("horizontalGrid");
+
     if (hgi)
     {
         x->setMinimum(hgi->minValue().toDouble());
         x->setMaximum(hgi->maxValue().toDouble());
     }
+
     x->setSingleStep(1.0);
     x->setValue(PLEConfigSkeleton::horizontalGrid());
     connect(skeleton, SIGNAL(horizontalGridChanged(double)), x, SLOT(setValue(double)));
@@ -60,11 +62,13 @@ GridSetupDialog::GridSetupDialog(QWidget * parent) :
 
     y = new QDoubleSpinBox(centralWidget);
     KConfigSkeletonItem * vgi = skeleton->findItem("verticalGrid");
-    if (vgi)
+
+    if (vgi && hgi)
     {
         y->setMinimum(hgi->minValue().toDouble());
         y->setMaximum(hgi->maxValue().toDouble());
     }
+
     y->setSingleStep(1.0);
     y->setValue(PLEConfigSkeleton::verticalGrid());
     connect(skeleton, SIGNAL(verticalGridChanged(double)), y, SLOT(setValue(double)));
