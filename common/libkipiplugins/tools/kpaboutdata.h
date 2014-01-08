b2b7a1a3addf2707a58af071b66c35af1fe0d424
Gilles Caulier <caulier.gilles@gmail.com>
2014-01-08 11:31:32 +0100

    (c) team to 2014

--- common/libkipiplugins/tools/kpaboutdata.h
+++ common/libkipiplugins/tools/kpaboutdata.h
@@ -6,8 +6,8 @@
  * Date        : 2006-14-09
  * Description : Kipi-Plugins shared library.
  *
- * Copyright (C) 2006-2010 Angelo Naselli <anaselli at linux dot it>
- * Copyright (C) 2010-2013 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2010 by Angelo Naselli <anaselli at linux dot it>
+ * Copyright (C) 2010-2014 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -48,10 +48,10 @@ class KIPIPLUGINS_EXPORT KPAboutData : public QObject, public KAboutData
 public:
 
     explicit KPAboutData(const KLocalizedString& pluginName,
-                        const QByteArray& pluginVersion            = QByteArray(),
-                        enum  LicenseKey licenseType               = License_Unknown,
-                        const KLocalizedString& pluginDescription  = KLocalizedString(),
-                        const KLocalizedString& copyrightStatement = ki18n("Copyright 2003-2013, kipi-plugins team"));
+                         const QByteArray& pluginVersion            = QByteArray(),
+                         enum  LicenseKey licenseType               = License_Unknown,
+                         const KLocalizedString& pluginDescription  = KLocalizedString(),
+                         const KLocalizedString& copyrightStatement = ki18n("Copyright 2003-2014, kipi-plugins team"));
 
     KPAboutData(const KPAboutData& other);
     virtual ~KPAboutData();
