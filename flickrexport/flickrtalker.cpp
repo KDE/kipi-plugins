1d4a95bc3d36060a025b8950aaaca467253532bc
Gilles Caulier <caulier.gilles@gmail.com>
2014-07-28 07:57:11 +0200

    use SSL based url for flickr web service communication BUGS: 336835 FIXED-IN: 4.2.0

--- flickrexport/flickrtalker.cpp
+++ flickrexport/flickrtalker.cpp
@@ -7,7 +7,7 @@
  * Description : a kipi plugin to export images to Flickr web service
  *
  * Copyright (C) 2005-2009 by Vardhman Jain <vardhman at gmail dot com>
- * Copyright (C) 2009-2013 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2009-2014 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -106,9 +106,9 @@ FlickrTalker::FlickrTalker(QWidget* const parent, const QString& serviceName)
     }
     else
     {
-        m_apiUrl    = QString("http://www.flickr.com/services/rest/");
-        m_authUrl   = QString("http://www.flickr.com/services/auth/");
-        m_uploadUrl = QString("http://api.flickr.com/services/upload/");
+        m_apiUrl    = QString("https://www.flickr.com/services/rest/");
+        m_authUrl   = QString("https://www.flickr.com/services/auth/");
+        m_uploadUrl = QString("https://api.flickr.com/services/upload/");
 
         m_apikey    = "49d585bafa0758cb5c58ab67198bf632";
         m_secret    = "34b39925e6273ffd";
