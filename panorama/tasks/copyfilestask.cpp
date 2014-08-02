fe279dc1be18b57854fae4476f2a10e7c4cc9c7f
Gilles Caulier <caulier.gilles@gmail.com>
2014-08-02 07:10:06 +0200

    Panorama Tool : Store common metadata info as ExpoBlending tool

--- panorama/tasks/copyfilestask.cpp
+++ panorama/tasks/copyfilestask.cpp
@@ -44,27 +44,63 @@ CopyFilesTask::CopyFilesTask(QObject* parent, const KUrl& workDir, const KUrl& p
                              const KUrl& ptoUrl, const ItemUrlsMap& urls, bool savePTO, bool addGPlusMetadata)
     : Task(parent, COPY, workDir), panoUrl(panoUrl), finalPanoUrl(finalPanoUrl),
       ptoUrl(ptoUrl), urlList(&urls), savePTO(savePTO), addGPlusMetadata(addGPlusMetadata)
-{}
+{
+}
 
 CopyFilesTask::CopyFilesTask(const KUrl& workDir, const KUrl& panoUrl, const KUrl& finalPanoUrl,
                              const KUrl& ptoUrl, const ItemUrlsMap& urls, bool savePTO, bool addGPlusMetadata)
     : Task(0, COPY, workDir), panoUrl(panoUrl), finalPanoUrl(finalPanoUrl),
       ptoUrl(ptoUrl), urlList(&urls), savePTO(savePTO), addGPlusMetadata(addGPlusMetadata)
-{}
+{
+}
 
 CopyFilesTask::~CopyFilesTask()
-{}
+{
+}
 
 void CopyFilesTask::run()
 {
-    QFile panoFile(panoUrl.toLocalFile());
-    QFile finalPanoFile(finalPanoUrl.toLocalFile());
+    QFile     panoFile(panoUrl.toLocalFile());
+    QFile     finalPanoFile(finalPanoUrl.toLocalFile());
 
     QFileInfo fi(finalPanoUrl.toLocalFile());
-    KUrl finalPTOUrl(finalPanoUrl);
+    KUrl      finalPTOUrl(finalPanoUrl);
     finalPTOUrl.setFileName(fi.completeBaseName() + ".pto");
-    QFile ptoFile(ptoUrl.toLocalFile());
-    QFile finalPTOFile(finalPTOUrl.toLocalFile());
+
+    QFile     ptoFile(ptoUrl.toLocalFile());
+    QFile     finalPTOFile(finalPTOUrl.toLocalFile());
+    
+    if (!panoFile.exists())
+    {
+        errString = i18n("Temporary panorama file does not exists.");
+        kDebug() << "Temporary panorama file does not exists: " + panoUrl.toLocalFile();
+        successFlag = false;
+        return;
+    }
+
+    if (finalPanoFile.exists())
+    {
+        errString = i18n("A file named %1 already exists.", finalPanoUrl.fileName());
+        kDebug() << "Final panorama file already exists: " + finalPanoUrl.toLocalFile();
+        successFlag = false;
+        return;
+    }
+    
+    if (savePTO && !ptoFile.exists())
+    {
+        errString = i18n("Temporary project file does not exist.");
+        kDebug() << "Temporary project file does not exists: " + ptoUrl.toLocalFile();
+        successFlag = false;
+        return;
+    }
+    
+    if (savePTO && finalPTOFile.exists())
+    {
+        errString = i18n("A file named %1 already exists.", finalPTOUrl.fileName());
+        kDebug() << "Final project file already exists: " + finalPTOUrl.toLocalFile();
+        successFlag = false;
+        return;
+    }
 
     kDebug() << "Copying GPS info...";
       
@@ -87,37 +123,29 @@ void CopyFilesTask::run()
             break;
         }        
     }
+
+    // Restore usual and common metadata from first shot.
     
-    if (!panoFile.exists())
-    {
-        errString = i18n("Temporary panorama file does not exists.");
-        kDebug() << "Temporary panorama file does not exists: " + panoUrl.toLocalFile();
-        successFlag = false;
-        return;
-    }
-    if (finalPanoFile.exists())
-    {
-        errString = i18n("A file named %1 already exists.", finalPanoUrl.fileName());
-        kDebug() << "Final panorama file already exists: " + finalPanoUrl.toLocalFile();
-        successFlag = false;
-        return;
-    }
-    if (savePTO && !ptoFile.exists())
-    {
-        errString = i18n("Temporary project file does not exist.");
-        kDebug() << "Temporary project file does not exists: " + ptoUrl.toLocalFile();
-        successFlag = false;
-        return;
-    }
-    if (savePTO && finalPTOFile.exists())
-    {
-        errString = i18n("A file named %1 already exists.", finalPTOUrl.fileName());
-        kDebug() << "Final project file already exists: " + finalPTOUrl.toLocalFile();
-        successFlag = false;
-        return;
-    }
+    KPMetadata metaSrc(urlList->constBegin().key().toLocalFile());
+    KPMetadata metaDst(panoUrl.toLocalFile());
+    metaDst.setIptc(metaSrc.getIptc());
+    metaDst.setXmp(metaSrc.getXmp());
+    metaDst.setXmpTagString("Xmp.tiff.Make",  metaSrc.getExifTagString("Exif.Image.Make"));
+    metaDst.setXmpTagString("Xmp.tiff.Model", metaSrc.getExifTagString("Exif.Image.Model"));
+    
+    QString filesList;
+    
+    for (ItemUrlsMap::const_iterator i = urlList->constBegin(); i != urlList->constEnd(); ++i)
+        filesList.append(i.key().fileName() + " ; ");
+
+    filesList.truncate(filesList.length()-3);
+    
+    metaDst.setXmpTagString("Xmp.kipi.PanoramaInputFiles", filesList, false);
+    metaDst.setImageDateTime(QDateTime::currentDateTime());
+    metaDst.applyChanges();    
     
     kDebug() << "Copying panorama file...";
+    
     if (!panoFile.copy(finalPanoUrl.toLocalFile()) || !panoFile.remove())
     {
         errString = i18n("Cannot move panorama from %1 to %2.",
@@ -143,6 +171,7 @@ void CopyFilesTask::run()
     if (savePTO)
     {
         kDebug() << "Copying project file...";
+    
         if (!ptoFile.copy(finalPTOUrl.toLocalFile()))
         {
             errString = i18n("Cannot move project file from %1 to %2.",
@@ -153,6 +182,7 @@ void CopyFilesTask::run()
         }
 
         kDebug() << "Copying converted RAW files...";
+        
         for (ItemUrlsMap::const_iterator i = urlList->constBegin(); i != urlList->constEnd(); ++i)
         {
             if (KPMetadata::isRawFile(i.key()))
@@ -160,6 +190,7 @@ void CopyFilesTask::run()
                 KUrl finalImgUrl(finalPanoUrl);
                 finalImgUrl.setFileName(i->preprocessedUrl.fileName());
                 QFile imgFile(i->preprocessedUrl.toLocalFile());
+        
                 if (!imgFile.copy(finalImgUrl.toLocalFile()))
                 {
                     errString = i18n("Cannot copy converted image file from %1 to %2.",
