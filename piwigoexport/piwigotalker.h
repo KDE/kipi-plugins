9220fdfb92912042e0f0329836ab661a9d5dbcd1
Frédéric Coiffier <fcoiffie@gmail.com>
2014-09-30 15:43:33 +0200

    Update copyrights

--- piwigoexport/piwigotalker.h
+++ piwigoexport/piwigotalker.h
@@ -3,14 +3,14 @@
 * This file is a part of kipi-plugins project
 * http://www.digikam.org
 *
-* Date        : 2010-02-15
+* Date        : 2014-09-30
 * Description : a plugin to export to a remote Piwigo server.
 *
 * Copyright (C) 2003-2005 by Renchi Raju <renchi dot raju at gmail dot com>
 * Copyright (C) 2006      by Colin Guthrie <kde at colin dot guthr dot ie>
 * Copyright (C) 2006-2013 by Gilles Caulier <caulier dot gilles at gmail dot com>
 * Copyright (C) 2008      by Andrea Diamantini <adjam7 at gmail dot com>
-* Copyright (C) 2010      by Frederic Coiffier <frederic dot coiffier at free dot com>
+* Copyright (C) 2010-2014 by Frederic Coiffier <frederic dot coiffier at free dot com>
 *
 * This program is free software; you can redistribute it
 * and/or modify it under the terms of the GNU General
