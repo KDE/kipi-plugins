e87eb607f379e46b53c40c1acc94133729873800
Gilles Caulier <caulier.gilles@gmail.com>
2014-10-04 15:14:50 +0200

    fix cppcheck warnings

--- imageviewer/viewerwidget.cpp
+++ imageviewer/viewerwidget.cpp
@@ -7,7 +7,7 @@
  * Description : a kipi plugin to show image using an OpenGL interface.
  *
  * Copyright (C) 2007-2008 by Markus Leuthold <kusi at forum dot titlis dot org>
- * Copyright (C) 2008-2013 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2008-2014 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -109,6 +109,12 @@ public:
         if (pl)
         {
             iface = pl->interface();
+        }
+
+        for (int i = 0; i < CACHESIZE ; i++)
+        {
+            cache[i].file_index = 0;
+            cache[i].texture    = 0;
         }
     }
 
