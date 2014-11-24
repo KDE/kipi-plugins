ce7b13c5a109cc5fede0806678b8f90e9b84848a
Gilles Caulier <caulier.gilles@gmail.com>
2014-11-24 17:22:34 +0100

    add new macro to detect libkexiv2 and libkdcraw as local or shared lib

--- cmake/modules/MacroLocalLibs.cmake
+++ cmake/modules/MacroLocalLibs.cmake
@@ -11,7 +11,7 @@ macro(DETECT_LIBKIPI MIN_VERSION)
 
         message(STATUS "libkipi : search system based library")
         find_package(KF5Kipi ${MIN_VERSION})
-        set(KF5Kipi_LIBRARIES KF5::Kipi PARENT_SCOPE)
+        set(KF5Kipi_LIBRARIES KF5::Kipi)
 
     else()
 
@@ -29,5 +29,59 @@ macro(DETECT_LIBKIPI MIN_VERSION)
 
     message(STATUS "libkipi include dir : ${KF5Kipi_INCLUDE_DIRS}")
     message(STATUS "libkipi library     : ${KF5Kipi_LIBRARIES}")
+
+endmacro()
+
+macro(DETECT_LIBKDCRAW MIN_VERSION)
+
+    if (NOT DIGIKAMSC_COMPILE_LIBKDCRAW)
+
+        message(STATUS "libkdcraw : search system based library")
+        find_package(KF5KDcraw ${MIN_VERSION})
+        set(KF5KDcraw_LIBRARIES KF5::KDcraw)
+
+    else()
+
+        message(STATUS "libkdcraw : use local library from ${CMAKE_SOURCE_DIR}/extra/libkdcraw/")
+        find_file(KF5KDcraw_FOUND CMakeLists.txt PATHS ${CMAKE_SOURCE_DIR}/extra/libkdcraw/)
+
+        if(NOT KF5KDcraw_FOUND)
+            message(ERROR "libkdcraw : local library not found")
+        endif()
+
+        set(KF5KDcraw_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/extra/libkdcraw/src ${CMAKE_BINARY_DIR}/extra/libkdcraw/src ${CMAKE_BINARY_DIR}/extra/libkdcraw)
+        set(KF5KDcraw_LIBRARIES KF5KDcraw)
+
+    endif()
+
+    message(STATUS "libkdcraw include dir : ${KF5KDcraw_INCLUDE_DIRS}")
+    message(STATUS "libkdcraw library     : ${KF5KDcraw_LIBRARIES}")
+
+endmacro()
+
+macro(DETECT_LIBKEXIV2 MIN_VERSION)
+
+    if (NOT DIGIKAMSC_COMPILE_LIBKEXIV2)
+
+        message(STATUS "libkexiv2 : search system based library")
+        find_package(KF5KExiv2 ${MIN_VERSION})
+        set(KF5KExiv2_LIBRARIES KF5::KExiv2)
+
+    else()
+
+        message(STATUS "libkexiv2 : use local library from ${CMAKE_SOURCE_DIR}/extra/libkexiv2/")
+        find_file(KF5KExiv2_FOUND CMakeLists.txt PATHS ${CMAKE_SOURCE_DIR}/extra/libkexiv2/)
+
+        if(NOT KF5KExiv2_FOUND)
+            message(ERROR "libkexiv2 : local library not found")
+        endif()
+
+        set(KF5KExiv2_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/extra/libkexiv2/src ${CMAKE_BINARY_DIR}/extra/libkexiv2/src ${CMAKE_BINARY_DIR}/extra/libkexiv2)
+        set(KF5KExiv2_LIBRARIES KF5KExiv2)
+
+    endif()
+
+    message(STATUS "libkexiv2 include dir : ${KF5KExiv2_INCLUDE_DIRS}")
+    message(STATUS "libkexiv2 library     : ${KF5KExiv2_LIBRARIES}")
 
 endmacro()
