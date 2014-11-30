72c3414cf61af9df8cea96d3072d336d84555511
Gilles Caulier <caulier.gilles@gmail.com>
2014-11-30 12:47:23 +0100

    use same logic with all lib than libkipi about to handle include dirs. print on the console cmake var for info

--- cmake/modules/MacroLocalLibs.cmake
+++ cmake/modules/MacroLocalLibs.cmake
@@ -6,6 +6,7 @@
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
 macro(DETECT_LIBKIPI MIN_VERSION)
+
     if (NOT DIGIKAMSC_COMPILE_LIBKIPI)
 
         message(STATUS "libkipi : search system based library")
@@ -30,11 +31,12 @@ macro(DETECT_LIBKIPI MIN_VERSION)
 
     endif()
 
-    message(STATUS "libkipi found       : ${KF5Kipi_FOUND}")
-    message(STATUS "libkipi library     : ${KF5Kipi_LIBRARIES}")
+    message(STATUS "libkipi found         : ${KF5Kipi_FOUND}")
+    message(STATUS "libkipi library       : ${KF5Kipi_LIBRARIES}")
+    message(STATUS "libkipi includes      : ${KF5Kipi_INCLUDE_DIRS}")
 
-
-    # detect libkipi so version used to compile kipi tool to identify if plugin can be loaded in memory by libkipi.
+    # Detect libkipi so version used to compile kipi tool to identify if plugin can be loaded in memory by libkipi.
+    # This will be used to populate plugin desktop files.
 
     find_file(KF5KipiConfig_FOUND libkipi_config.h PATHS ${KF5Kipi_INCLUDE_DIRS})
     file(READ "${KF5KipiConfig_FOUND}" KIPI_CONFIG_H_CONTENT)
@@ -60,8 +62,7 @@ macro(DETECT_LIBKDCRAW MIN_VERSION)
 
         if(KF5KDcraw_FOUND)
             set(KF5KDcraw_LIBRARIES KF5::KDcraw)
-        else()
-            set(KF5KDcraw_FOUND FALSE)
+            get_target_property(KF5KDcraw_INCLUDE_DIRS KF5::KDcraw INTERFACE_INCLUDE_DIRECTORIES)
         endif()
 
     else()
@@ -80,6 +81,7 @@ macro(DETECT_LIBKDCRAW MIN_VERSION)
 
     message(STATUS "libkdcraw found       : ${KF5KDcraw_FOUND}")
     message(STATUS "libkdcraw library     : ${KF5KDcraw_LIBRARIES}")
+    message(STATUS "libkdcraw includes    : ${KF5KDcraw_INCLUDE_DIRS}")
 
 endmacro()
 
@@ -92,8 +94,7 @@ macro(DETECT_LIBKEXIV2 MIN_VERSION)
 
         if(KF5KExiv2_FOUND)
             set(KF5KExiv2_LIBRARIES KF5::KExiv2)
-        else()
-            set(KF5KExiv2_FOUND FALSE)
+            get_target_property(KF5KExiv2_INCLUDE_DIRS KF5::KExiv2 INTERFACE_INCLUDE_DIRECTORIES)
         endif()
 
     else()
@@ -112,6 +113,7 @@ macro(DETECT_LIBKEXIV2 MIN_VERSION)
 
     message(STATUS "libkexiv2 found       : ${KF5KExiv2_FOUND}")
     message(STATUS "libkexiv2 library     : ${KF5KExiv2_LIBRARIES}")
+    message(STATUS "libkexiv2 includes    : ${KF5KExiv2_INCLUDE_DIRS}")
 
 endmacro()
 
@@ -124,8 +126,7 @@ macro(DETECT_LIBKFACE MIN_VERSION)
 
         if(KF5KFace_FOUND)
             set(KF5KFace_LIBRARIES KF5::KFace)
-        else()
-            set(KF5KFace_FOUND FALSE)
+            get_target_property(KF5KFace_INCLUDE_DIRS KF5::KFace INTERFACE_INCLUDE_DIRECTORIES)
         endif()
 
     else()
@@ -142,8 +143,9 @@ macro(DETECT_LIBKFACE MIN_VERSION)
 
     endif()
 
-    message(STATUS "libkface found       : ${KF5KFace_FOUND}")    
-    message(STATUS "libkface library     : ${KF5KFace_LIBRARIES}")
+    message(STATUS "libkface found        : ${KF5KFace_FOUND}")
+    message(STATUS "libkface library      : ${KF5KFace_LIBRARIES}")
+    message(STATUS "libkface includes     : ${KF5KFace_INCLUDE_DIRS}")
 
 endmacro()
 
@@ -156,8 +158,7 @@ macro(DETECT_LIBKGEOMAP MIN_VERSION)
 
         if(KF5KGeomap_FOUND)
             set(KF5KGeomap_LIBRARIES KF5::KGeomap)
-        else()
-            set(KF5KGeomap_FOUND FALSE)
+            get_target_property(KF5KGeomap_INCLUDE_DIRS KF5::KGeomap INTERFACE_INCLUDE_DIRECTORIES)
         endif()
 
     else()
@@ -174,7 +175,8 @@ macro(DETECT_LIBKGEOMAP MIN_VERSION)
 
     endif()
 
-    message(STATUS "libkgeomap found       : ${KF5KGeomap_FOUND}")
-    message(STATUS "libkgeomap library     : ${KF5KGeomap_LIBRARIES}")
+    message(STATUS "libkgeomap found      : ${KF5KGeomap_FOUND}")
+    message(STATUS "libkgeomap library    : ${KF5KGeomap_LIBRARIES}")
+    message(STATUS "libkgeomap includes   : ${KF5KGeomap_INCLUDE_DIRS}")
 
 endmacro()
