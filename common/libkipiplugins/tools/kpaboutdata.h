89cea1aed91a798b40eda688297df14cdd006498
Gilles Caulier <caulier.gilles@gmail.com>
2014-12-31 16:46:47 +0100

    (c) 2015

--- common/libkipiplugins/tools/kpaboutdata.h
+++ common/libkipiplugins/tools/kpaboutdata.h
@@ -7,7 +7,7 @@
  * Description : Kipi-Plugins shared library.
  *
  * Copyright (C) 2006-2010 by Angelo Naselli <anaselli at linux dot it>
- * Copyright (C) 2010-2014 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2010-2015 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -51,7 +51,7 @@ public:
                          const QByteArray& pluginVersion            = QByteArray(),
                          enum  LicenseKey licenseType               = License_Unknown,
                          const KLocalizedString& pluginDescription  = KLocalizedString(),
-                         const KLocalizedString& copyrightStatement = ki18n("Copyright 2003-2014, kipi-plugins team"));
+                         const KLocalizedString& copyrightStatement = ki18n("Copyright 2003-2015, digiKam developers team"));
 
     KPAboutData(const KPAboutData& other);
     virtual ~KPAboutData();
