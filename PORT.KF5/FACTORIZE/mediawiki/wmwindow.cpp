8c86f8c4c13184c198d415eca9f29ed10cc83a34
Gilles Caulier <caulier.gilles@gmail.com>
2014-12-31 09:02:49 -0500

    (c) 2015

--- PORT.KF5/FACTORIZE/mediawiki/wmwindow.cpp
+++ PORT.KF5/FACTORIZE/mediawiki/wmwindow.cpp
@@ -7,7 +7,7 @@
  * Description : A kipi plugin to export images to a MediaWiki wiki
  *
  * Copyright (C) 2011      by Alexandre Mendes <alex dot mendes1988 at gmail dot com>
- * Copyright (C) 2011-2014 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2011-2015 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2012      by Parthasarathy Gopavarapu <gparthasarathy93 at gmail dot com>
  * Copyright (C) 2012-2013 by Peter Potrowl <peter dot potrowl at gmail dot com>
  *
