0bbe75ba0268fcd4dfd7fef042c89d8d98eb8a30
Gilles Caulier <caulier.gilles@gmail.com>
2015-08-23 15:28:37 +0200

    fix CR #1317073

--- kmlexport/plugin_kmlexport.cpp
+++ kmlexport/plugin_kmlexport.cpp
@@ -60,6 +60,9 @@ K_EXPORT_PLUGIN ( KMLExportFactory("kipiplugin_kmlexport") )
 Plugin_KMLExport::Plugin_KMLExport(QObject* const parent, const QVariantList&)
     : Plugin( /*KMLExportFactory::componentData(),*/ parent, "KMLExport")
 {
+    m_actionKMLExport = 0;
+    m_interface       = 0;
+    
     qCDebug(KIPIPLUGINS_LOG) << "Plugin_SendImages plugin loaded";
 
     setUiBaseName("kipiplugin_kmlexportui.rc");
