28ac353f5b0d6be141d9a37f569a71f2837fe276
Shourya Singh Gupta <shouryasgupta@gmail.com>
2015-10-12 13:30:40 +0530

    Generating random strings, in MPForm of Facebook, using newly created class in kputil.cpp and removing krandom.h dependency.

--- facebook/mpform.cpp
+++ facebook/mpform.cpp
@@ -37,19 +37,16 @@
 #include <QUrl>
 #include <QString>
 
-// KDE includes
-
-#include <krandom.h>
-
 // Local includes
 
 #include "kipiplugins_debug.h"
+#include "kputil.h"
 
 namespace KIPIFacebookPlugin
 {
 
 MPForm::MPForm()
-    : m_boundary(KRandom::randomString(42 + 13).toLatin1())
+    : m_boundary(KIPIPlugins::KPRandomGenerator::randomString(42 + 13).toLatin1())
 {
     reset();
 }
