1c7c1478ca5a2762e0e68ac3bbf1a6410227be6e
Gilles Caulier <caulier.gilles@gmail.com>
2016-02-26 08:28:27 +0059

    polish

--- advancedslideshow/common/kpsvgpixmaprenderer.cpp
+++ advancedslideshow/common/kpsvgpixmaprenderer.cpp
@@ -1,39 +1,44 @@
-/*
- * This file is a part of kipi-plugins project
- * Copyright 2015  Alexander Potashev <aspotashev@gmail.com>
+/* ============================================================
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License or (at your option) version 3 or any later version
- * accepted by the membership of KDE e.V. (or its successor approved
- * by the membership of KDE e.V.), which shall act as a proxy
- * defined in Section 14 of version 3 of the license.
+ * This file is a part of kipi-plugins project
+ * http://www.digikam.org
+ *
+ * Date        : 2015-09-09
+ * Description : a pixmap to render KIPI svg logo.
+ *
+ * Copyright (C) 2015 by Alexander Potashev <aspotashev at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+ * ============================================================ */
 
 #include "kpsvgpixmaprenderer.h"
+
+// Qt includes
 
 #include <QStandardPaths>
 #include <QPainter>
 #include <QSvgRenderer>
 
+namespace KIPIAdvancedSlideshowPlugin
+{
+
 KPSvgPixmapRenderer::KPSvgPixmapRenderer(int width, int height, const QString& filename)
-    : m_width(width)
-    , m_height(height)
-    , m_filename(filename)
+    : m_width(width),
+      m_height(height),
+      m_filename(filename)
 {
     if (m_filename.isEmpty())
     {
-        m_filename = QStandardPaths::locate(QStandardPaths::GenericDataLocation,
-                                            QString::fromLatin1("kf5/kipi/pics/kipi-icon.svg"));
+        m_filename = QString::fromLatin1(":/kipi-icon.svg");
     }
 }
 
@@ -48,3 +53,5 @@ QPixmap KPSvgPixmapRenderer::getPixmap() const
 
     return pixmap;
 }
+
+} // namespace KIPIAdvancedSlideshowPlugin
