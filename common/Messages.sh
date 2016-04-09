a54e3fd7ccbe15f225432ba54e71c11018ef5172
Burkhard Lück <lueck@hube-lueck.de>
2016-04-09 14:03:18 +0200

    Tips file has been removed, do no track strings CCMAIL:i18n-infrastructure@kde.org

--- common/Messages.sh
+++ common/Messages.sh
@@ -1,4 +1,2 @@
 #! /bin/sh
-(cd data && $PREPARETIPS > ../tips.cpp)
 $XGETTEXT tips.cpp `find libkipiplugins -name \*.cpp -o -name \*.h -o -name \*.h.cmake` -o $podir/kipiplugins.pot
-rm -f tips.cpp
