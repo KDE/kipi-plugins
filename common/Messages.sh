9657e5c7ce8624665f01bd5b29d12f6f863a936c
Pino Toscano <pino@kde.org>
2016-04-10 10:59:22 +0200

    i18n: do not try to use tips.cpp
    Since commit a54e3fd7ccbe15f225432ba54e71c11018ef5172 is no more
    generated.

--- common/Messages.sh
+++ common/Messages.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-$XGETTEXT tips.cpp `find libkipiplugins -name \*.cpp -o -name \*.h -o -name \*.h.cmake` -o $podir/kipiplugins.pot
+$XGETTEXT `find libkipiplugins -name \*.cpp -o -name \*.h -o -name \*.h.cmake` -o $podir/kipiplugins.pot
