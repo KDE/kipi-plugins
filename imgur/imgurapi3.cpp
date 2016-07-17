c9429d8ddfe438c5760d6dc6364dc16bdb9440b7
Maik Qualmann <metzpinguin@gmail.com>
2016-07-17 12:55:02 +0200

    add copyright

--- imgur/imgurapi3.cpp
+++ imgur/imgurapi3.cpp
@@ -1,3 +1,25 @@
+/* ============================================================
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.digikam.org
+ *
+ * Date        : 2016-05-27
+ * Description : Implementation of v3 of the Imgur API
+ *
+ * Copyright (C) 2016 by Fabian Vogt <fabian at ritter dash vogt dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
 #include <klocalizedstring.h>
 
 #include <QDebug>
