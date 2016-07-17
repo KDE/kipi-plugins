1ca948503bb095df1334131481811a2141697edf
Maik Qualmann <metzpinguin@gmail.com>
2016-07-17 12:30:53 +0200

    apply patch #99897 from Fabian Vogt to add o2 library from https://github.com/pipacs/o2 CCBUGS: 360404

---
+++ common/libkipiplugins/o2/src/o0abstractstore.h
@@ -0,0 +1,22 @@
+#ifndef O0ABSTRACTSTORE_H
+#define O0ABSTRACTSTORE_H
+
+#include <QObject>
+#include <QString>
+
+/// Storage for strings.
+class O0AbstractStore: public QObject {
+    Q_OBJECT
+
+public:
+    explicit O0AbstractStore(QObject *parent = 0): QObject(parent) {
+    }
+
+    /// Retrieve a string value by key.
+    virtual QString value(const QString &key, const QString &defaultValue = QString()) = 0;
+
+    /// Set a string value for a key.
+    virtual void setValue(const QString &key, const QString &value) = 0;
+};
+
+#endif // O0ABSTRACTSTORE_H
---
+++ common/libkipiplugins/o2/src/o2facebook.h
@@ -0,0 +1,18 @@
+#ifndef O2FACEBOOK_H
+#define O2FACEBOOK_H
+
+#include "o2.h"
+
+/// Facebook's dialect of OAuth 2.0
+class O2Facebook: public O2 {
+    Q_OBJECT
+
+public:
+    explicit O2Facebook(QObject *parent = 0);
+
+protected Q_SLOTS:
+    void onVerificationReceived(QMap<QString, QString>);
+    virtual void onTokenReplyFinished();
+};
+
+#endif // O2FACEBOOK_H
