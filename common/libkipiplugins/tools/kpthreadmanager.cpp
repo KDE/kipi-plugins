6c56ff08c687608bdabcace52afe22687571a683
Gilles Caulier <caulier.gilles@gmail.com>
2017-05-21 10:30:18 +0200

    fix headers

--- common/libkipiplugins/tools/kpthreadmanager.cpp
+++ common/libkipiplugins/tools/kpthreadmanager.cpp
@@ -1,24 +1,19 @@
-/** ===========================================================
- * @file
+/* ============================================================
  *
- * This file is a part of digiKam project
- * <a href="http://www.digikam.org">http://www.digikam.org</a>
+ * This file is a part of kipi-plugins project
+ * http://www.digikam.org
  *
- * @date   2011-12-28
- * @brief  Low level threads management for batch processing on multi-core
+ * Date        : 2011-12-28
+ * Description : Low level threads management for batch processing on multi-core
  *
- * @author Copyright (C) 2011-2017 by Gilles Caulier
- *         <a href="mailto:caulier dot gilles at gmail dot com">caulier dot gilles at gmail dot com</a>
- * @author Copyright (C) 2014 by Veaceslav Munteanu
- *         <a href="mailto:veaceslav dot munteanu90 at gmail dot com">veaceslav dot munteanu90 at gmail dot com</a>
- * @author Copyright (C) 2011-2012 by A Janardhan Reddy
- *         <a href="annapareddyjanardhanreddy at gmail dot com">annapareddyjanardhanreddy at gmail dot com</a>
+ * Copyright (C)      2014 by Veaceslav Munteanu <veaceslav dot munteanu90 at gmail dot com>
+ * Copyright (C) 2011-2017 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2011-2012 by A Janardhan Reddy <annapareddyjanardhanreddy at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option)
- * any later version.
+ * either version 2, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -228,4 +223,4 @@ void KPThreadManager::run()
     }
 }
 
-}  // namespace KIPIPlugins
+} // namespace KIPIPlugins
